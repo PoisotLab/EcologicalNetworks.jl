b68c652473ef5775d334f7465bdcb97d4d68bd99
Timothée Poisot <tim@poisotlab.io>
2014-10-21 17:23:49 +1300

    Fixed the null model
    And I've been smart about it too!

--- src/nullmodels.jl
+++ src/nullmodels.jl
@@ -13,7 +13,8 @@ Type III out
 
 =#
 function null3out(A::Array{Float64, 2})
-   return degree_out(A) ./ size(A)[2]
+   p_rows = degree_out(A) ./ size(A)[2]
+   return hcat(map((x) -> p_rows, [1:size(A)[1]])...)
 end
 
 #=
@@ -22,7 +23,7 @@ Type III in
 
 =#
 function null3in(A::Array{Float64, 2})
-   return degree_in(A) ./ size(A)[1]
+   return null3out(A')' # I don't work hard, so I work smart
 end
 
 #=
