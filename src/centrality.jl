7603699ad71b3c9e0655148b992c1c79f580ac2a
Timothée Poisot <tim@poisotlab.io>
2015-03-05 16:57:49 -0700

    Updated centrality to get rid of deprecation

--- src/centrality.jl
+++ src/centrality.jl
@@ -2,6 +2,6 @@ function centrality_katz(A::Array{Float64,2}; a::Float64=0.1, k::Int64=5)
    @assert a <= 1.0
    @assert a >= 0.0
    @assert k >= 1
-   centr = sum(hcat(map((x) -> vec(sum((a^x).*(A^x),1)), [1:k])...),2)
+   centr = sum(hcat(map((x) -> vec(sum((a^x).*(A^x),1)), [1:k;])...),2)
    return centr ./ sum(centr)
 end
