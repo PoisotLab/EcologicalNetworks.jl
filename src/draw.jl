04ec045dfadbb764de02b6278abd8f09eeed5161
Timothée Poisot <tim@poisotlab.io>
2015-03-06 09:51:07 -0700

    Commenting style

--- src/draw.jl
+++ src/draw.jl
@@ -2,17 +2,17 @@ function draw_matrix(A::Array{Float64,2}; transform = (x) -> 1-x, file="pen.png"
    nbot, ntop = size(A)
    width  = 4 + nbot*(10+4)
    height = 4 + ntop*(10+4)
-   ## Initialize device
+   # Initialize device
    c = CairoRGBSurface(width, height)
    cr = CairoContext(c)
    save(cr)
-   ## Background
+   # Background
    set_source_rgba(cr, 1.0, 1.0, 1.0, 1.0)
    rectangle(cr, 0.0, 0.0, float(width), float(height))
    fill(cr)
    restore(cr)
    save(cr)
-   ## Draw the blocks
+   # Draw the blocks
    for top in 1:ntop
       for bot in 1:nbot
          p = transform(A[bot,top])
