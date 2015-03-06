6fd0995cb6f97b317c7751de39a096e426da2f99
Timothée Poisot <tim@poisotlab.io>
2015-03-06 13:19:56 -0600

    Update main module file

--- src/ProbaNet.jl
+++ src/ProbaNet.jl
@@ -6,22 +6,30 @@ using Distributions
 using Cairo
 
 export nestedness, nestedness_axis,
-       # Measures of centrality
-       centrality_katz,
-       # Links and connectances
-       links, links_var, connectance,
-       # Mesaures of degree
-       degree_out, degree_in, degree_out_var, degree_in_var, degree, degree_var,
-       # Expected number of species with 0 interactions
-       species_is_free, free_species,
-       # Matrix manipulation utilities
-       make_unipartite, make_binary, make_bernoulli, nodiag,
-       # Probability algebra utilities
-       checkprob, i_esp, i_var, a_var, m_var,
-       # NAIVE !!! null models
-       null1, null2, null3out, null3in,
-       # Draw
-       draw_matrix
+
+  # Measures of centrality
+  centrality_katz,
+
+  # Links and connectances
+  links, links_var, connectance,
+
+  # Measures of degree
+  degree_out, degree_in, degree_out_var, degree_in_var, degree, degree_var,
+
+  # Expected number of species with 0 interactions
+  species_is_free, free_species,
+
+  # Matrix manipulation utilities
+  make_unipartite, make_binary, make_bernoulli, nodiag,
+
+  # Probability algebra utilities
+  checkprob, i_esp, i_var, a_var, m_var,
+
+  # NAIVE !!! null models
+  null1, null2, null3out, null3in,
+
+  # Draw
+  draw_matrix
 
 include(joinpath(".", "centrality.jl"))
 include(joinpath(".", "connectance.jl"))
@@ -33,4 +41,4 @@ include(joinpath(".", "proba_utils.jl"))
 include(joinpath(".", "nullmodels.jl"))
 include(joinpath(".", "draw.jl"))
 
-end # module
+end
