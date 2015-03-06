d121bc2c7476c685d3c1e56cd06e74d09d8da24a
Timothée Poisot <tim@poisotlab.io>
2015-03-05 17:00:58 -0700

    Added tests for Katz centrality

---
+++ test/centrality.jl
@@ -0,0 +1,23 @@
+module TestNullModels
+  using Base.Test
+  using ProbaNet
+
+  # Generate some data
+
+  A = [0.0 1.0 0.0 0.0; 0.0 0.0 0.0 1.0; 0.0 1.0 0.0 0.0; 0.0 0.0 0.0 0.0]
+
+  c_1_1 = centrality_katz(A, a=0.1, k=1)
+  c_2_1 = centrality_katz(A, a=0.2, k=1)
+  c_1_2 = centrality_katz(A, a=0.1, k=2)
+
+  e_c_1_1 = [0.0 0.625 0.0 0.375]
+  e_c_2_1 = [0.0 0.666 0.0 0.333]
+  e_c_1_2 = [0.0 0.625 0.0 0.374]
+
+  for i = 1:3
+    @test_approx_eq e_c_1_1[i] c_1_1[i]
+    @test_approx_eq e_c_2_1[i] c_2_1[i]
+    @test_approx_eq e_c_1_2[i] c_1_2[i]
+  end
+
+end
