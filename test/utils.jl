9a903abeb93fe3e29784abe19c03114e784e8af1
Timothée Poisot <tim@poisotlab.io>
2015-03-10 10:42:22 -0400

    Tests

--- test/utils.jl
+++ test/utils.jl
@@ -6,7 +6,7 @@ module TestUtilities
   @test_throws TypeError ProbabilisticNetwork.@checkprob Int64(1)
 
   # Probabilities must be in 0-1
-  #@test_throws DomainError ProbabilisticNetwork.@checkprob -0.2
-  #@test_throws DomainError ProbabilisticNetwork.@checkprob 1.2
+  @test_throws DomainError ProbabilisticNetwork.@checkprob -0.2
+  @test_throws DomainError ProbabilisticNetwork.@checkprob 1.2
 
 end
