ebaeefb572737aab6e957d97f89638cf2281d0f4
Timothée Poisot <tim@poisotlab.io>
2015-03-10 21:57:32 -0400

    Indentation

--- src/matrix_utils.jl
+++ src/matrix_utils.jl
@@ -2,29 +2,29 @@
 Makes a bipartite network unipartite
 =#
 function make_unipartite(A::Array{Float64,2})
-   S = sum(size(A))
-   B = zeros(Float64,(S,S))
-   B[1:size(A)[1],size(A)[1]+1:S] = A
-   return B
+  S = sum(size(A))
+  B = zeros(Float64,(S,S))
+  B[1:size(A)[1],size(A)[1]+1:S] = A
+  return B
 end
 
 #=
 Generates a random binary matrix based on a probabilistic one
 =#
 function make_bernoulli(A::Array{Float64,2})
-   return map((x) -> float64(rand(Bernoulli(x))), A)
+  return map((x) -> float64(rand(Bernoulli(x))), A)
 end
 
 #=
 Set all probabilities to 1.0
 =#
 function make_binary(A::Array{Float64,2})
-   return map((x) -> x>0.0 ? 1.0 : 0.0, A)
+  return map((x) -> x>0.0 ? 1.0 : 0.0, A)
 end
 
 #=
 Sets the diagonal to 0
 =#
 function nodiag(A::Array{Float64,2})
-   return A .* (1.0 .- eye(Float64, size(A)[1]))
+  return A .* (1.0 .- eye(Float64, size(A)[1]))
 end
