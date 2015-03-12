ee99d6bd8ee7e9a1b0677139ff19e1d504e956fd
Timothée Poisot <tim@poisotlab.io>
2015-03-12 13:38:34 -0400

    No call to Q during optimization
    Null model is only computed ONCE

--- src/modularity.jl
+++ src/modularity.jl
@@ -26,27 +26,44 @@ function Qr(A::Array{Float64, 2}, L::Array{Int64, 1})
 end
 
 #=
+New type
+=#
+type Partition
+  A::Array{Float64, 2}
+  L::Array{Int64, 1}
+  Q::Float64
+end
+
+#=
 Propagate labels edge by edge
 =#
 function propagate_labels(A::Array{Float64, 2}, kmax::Int64, smax::Int64)
   # Make a list of labels
   n_lab = maximum(size(A))
   L = vec(round(Int64, linspace(1, n_lab, n_lab)))
+  # Prepare the null model
+  m = links(A)
+  aij = A ./ (2*m)
+  ki = degree_out(A)
+  kj = degree_in(A)
+  kij = (ki .* kj') ./ ((2*m)*(2*m))
+  nmod = aij .- kij
+  best_Q = sum(nmod .* (L .== L')) # Initial modularity
   # Update
   no_increase = 0
   for k in 1:kmax
     i = rand(1:size(A)[1])
     j = rand(1:size(A)[2])
     if rand() <= A[i,j]
-      cQ = Q(A, L)
       cj = L[j]
       L[j] = L[i]
-      nQ = Q(A, L)
-      if nQ < cQ
+      tentative_Q = sum(nmod .* (L .== L')) # Tentative modularity
+      if tentative_Q < best_Q
         L[j] = cj
         no_increase = no_increase + 1
       else
         no_increase = 0
+        best_Q = tentative_Q
       end
     end
     if no_increase == smax
@@ -56,16 +73,7 @@ function propagate_labels(A::Array{Float64, 2}, kmax::Int64, smax::Int64)
       break
     end
   end
-  return (A, L)
-end
-
-#=
-New type
-=#
-type Partition
-  A::Array{Float64, 2}
-  L::Array{Int64, 1}
-  Q::Float64
+  return Partition(A, L, best_Q)
 end
 
 #=
@@ -77,22 +85,18 @@ function modularity(A::Array{Float64, 2}; replicates=100, kmax=0, smax=0, verbos
     kmax = 2 * prod(size(A))
     smax = maximum([round(Int64, kmax/5) 1])
   end
-  best_Q = 0.0
   # First trial
-  trial_partition = propagate_labels(A, kmax, smax)
-  partition = Partition(A, trial_partition[2], Q(trial_partition...))
+  best_partition = propagate_labels(A, kmax, smax)
   if verbose
     info(string("Modularity run 1 of ", replicates, ": Q=", partition.Q))
   end
   for trial in 2:replicates
     trial_partition = propagate_labels(A, kmax, smax)
-    trial_Q = Q(trial_partition...)
-    if trial_Q > best_Q
-      best_Q = trial_Q
-      partition = Partition(A, trial_partition[2], trial_Q)
+    if trial_partition.Q > best_partition.Q
+      best_partition = trial_partition
     end
     if verbose
-      info(string("Modularity run $trial of $replicates -- Q=$best_Q"))
+      info(string("Modularity run $trial of $replicates -- Q=", best_partition.Q))
     end
   end
   return partition
