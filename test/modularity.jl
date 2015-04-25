8d5084bec9f8112ae4788451b073cd20b698116b
Timothée Poisot <tim@poisotlab.io>
2015-04-25 18:04:14 -0400

    Additional tests for modularity

--- test/modularity.jl
+++ test/modularity.jl
@@ -42,4 +42,17 @@ module TestModularity
   A = eye(Float64, 3)
   @test_approx_eq modularity(A).Q 15/36
 
+  # Tests with a single module
+  A = eye(2)
+  mod = modularity(A)
+  mod.L = vec([1 1])
+  @test Qr(mod) == 0.0
+
+  # Test with a complete graph
+  A = ones(Float64, (3, 3))
+  mod = modularity(A)
+  @test length(unique(mod.L)) == 1
+  @test_approx_eq mod.Q 0.25000000000000006
+  @test Qr(mod) == 0.0
+
 end
