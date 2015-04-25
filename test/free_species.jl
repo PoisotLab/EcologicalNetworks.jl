32dc5d40f11fbd9c82861866da8571ba9732764c
Timothée Poisot <tim@poisotlab.io>
2015-04-25 17:57:06 -0400

    Tests of no pred / no succ

--- test/free_species.jl
+++ test/free_species.jl
@@ -13,4 +13,16 @@ module TestFreeSpecies
   # Make bipartite unipartite case
   @test_approx_eq free_species(B) 0.9
 
+  # No predecessors and successors
+  A = make_unipartite([1.0 0.4 0.3 0.8; 0.0 0.2 0.3 0.6; 0.1 0.2 0.4 0.2])
+
+  no_succ = species_has_no_successors(A)
+  no_pred = species_has_no_predecessors(A)
+
+  @test no_succ[1] == 0.0
+  @test no_succ[4] == 1.0
+
+  @test no_pred[1] == 1.0
+  @test no_pred[4] == 0.0
+
 end
