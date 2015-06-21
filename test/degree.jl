bfcb3ce5b52558129704de2942529b63fe744d47
Timothée Poisot <tim@poisotlab.io>
2015-06-21 11:11:35 -0400

    Tabs

--- test/degree.jl
+++ test/degree.jl
@@ -1,32 +1,32 @@
 module TestDegreeFunctions
-  using Base.Test
-  using ProbabilisticNetwork
+    using Base.Test
+    using ProbabilisticNetwork
 
-  # Generate some data
+    # Generate some data
 
-  A = [0.22 0.4; 0.3 0.1]
+    A = [0.22 0.4; 0.3 0.1]
 
-  d_in = vec([0.52 0.5])
-  d_out = vec([0.62 0.4])
-  d_tot = vec([1.14 0.9])
+    d_in = vec([0.52 0.5])
+    d_out = vec([0.62 0.4])
+    d_tot = vec([1.14 0.9])
 
-  Din = degree_in(A)
-  Dout = degree_out(A)
-  Dtot = degree(A)
+    Din = degree_in(A)
+    Dout = degree_out(A)
+    Dtot = degree(A)
 
-  Dov = degree_out_var(A)
-  @test_approx_eq Dov[2] 0.3
+    Dov = degree_out_var(A)
+    @test_approx_eq Dov[2] 0.3
 
-  Div = degree_in_var(A)
-  @test_approx_eq Div[1] 0.3816
+    Div = degree_in_var(A)
+    @test_approx_eq Div[1] 0.3816
 
-  Dv = degree_var(A)
-  @test_approx_eq Dv[2] 0.63
+    Dv = degree_var(A)
+    @test_approx_eq Dv[2] 0.63
 
-  for i in 1:2
-    @test_approx_eq d_in[i] Din[i]
-    @test_approx_eq d_out[i] Dout[i]
-    @test_approx_eq d_tot[i] Dtot[i]
-  end
+    for i in 1:2
+        @test_approx_eq d_in[i] Din[i]
+        @test_approx_eq d_out[i] Dout[i]
+        @test_approx_eq d_tot[i] Dtot[i]
+    end
 
 end
--- test/nestedness.jl
+++ test/nestedness.jl
@@ -1,16 +1,16 @@
 module TestNestedness
-  using Base.Test
-  using ProbabilisticNetwork
+    using Base.Test
+    using ProbabilisticNetwork
 
-  # Generate some data
+    # Generate some data
 
-  A = [1.0 0.0 0.0; 0.0 0.1 0.0; 0.0 0.0 0.1]
-  B = [1.0 1.0 1.0; 1.0 0.1 0.0; 1.0 0.0 0.0]
-  C = [1.0 1.0 1.0; 1.0 0.1 0.3; 0.4 0.2 0.0]
-  D = [1.0 1.0 1.0; 0.0 0.1 1.0; 0.0 0.0 1.0]
+    A = [1.0 0.0 0.0; 0.0 0.1 0.0; 0.0 0.0 0.1]
+    B = [1.0 1.0 1.0; 1.0 0.1 0.0; 1.0 0.0 0.0]
+    C = [1.0 1.0 1.0; 1.0 0.1 0.3; 0.4 0.2 0.0]
+    D = [1.0 1.0 1.0; 0.0 0.1 1.0; 0.0 0.0 1.0]
 
-  @test_approx_eq nestedness(A)[1] 0.0
-  @test_approx_eq nestedness(B)[1] 1.0
-  @test_approx_eq nestedness(C)[1] 0.9153846153846155
+    @test_approx_eq nestedness(A)[1] 0.0
+    @test_approx_eq nestedness(B)[1] 1.0
+    @test_approx_eq nestedness(C)[1] 0.9153846153846155
 
 end
