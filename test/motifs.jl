595213933b3dd96be15b76ec0bc89e2fc4e5a98d
Timothée Poisot <tim@poisotlab.io>
2015-07-24 23:43:15 +0200

    Fix tests following name change

--- test/motifs.jl
+++ test/motifs.jl
@@ -5,13 +5,13 @@ module TestMotifs
   # Test with a single link
   A = [0.2 0.8; 0.2 0.1]
   m = [0.0 1.0; 0.0 0.0]
-  pmotif = ProbabilisticNetwork.motif_internal(A, m)
+  pmotif = EcologicalNetwork.motif_internal(A, m)
   @test pmotif == vec([0.8 0.8 0.8 0.9])
 
   # Test with a perfect match
   A = [0.0 1.0; 0.0 0.0]
   m = [0.0 1.0; 0.0 0.0]
-  pmotif = ProbabilisticNetwork.motif_internal(A, m)
+  pmotif = EcologicalNetwork.motif_internal(A, m)
   @test pmotif == vec([1.0 1.0 1.0 1.0])
   @test motif_p(A, m) == 1.0
   @test motif_v(A, m) == 0.0
