806bb37f85de351ed841ab976214d8774626d9bd
Timothée Poisot <tim@poisotlab.io>
2016-11-20 17:35:47 -0500

    test for degree functions

--- test/degree.jl
+++ test/degree.jl
@@ -4,23 +4,23 @@ module TestDegreeFunctions
 
   # Generate some data
 
-  A = [0.22 0.4; 0.3 0.1]
+  N = UnipartiteProbaNetwork([0.22 0.4; 0.3 0.1])
 
   d_in = vec([0.52 0.5])
   d_out = vec([0.62 0.4])
   d_tot = vec([1.14 0.9])
 
-  Din = degree_in(A)
-  Dout = degree_out(A)
-  Dtot = degree(A)
+  Din = degree_in(N)
+  Dout = degree_out(N)
+  Dtot = degree(N)
 
-  Dov = degree_out_var(A)
+  Dov = degree_out_var(N)
   @test_approx_eq Dov[2] 0.3
 
-  Div = degree_in_var(A)
+  Div = degree_in_var(N)
   @test_approx_eq Div[1] 0.3816
 
-  Dv = degree_var(A)
+  Dv = degree_var(N)
   @test_approx_eq Dv[2] 0.63
 
   for i in 1:2
