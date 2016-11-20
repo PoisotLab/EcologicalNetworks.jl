4ed213a8b70947e3d7da35eabffbf5bf07eacfe9
Timothée Poisot <tim@poisotlab.io>
2016-11-20 13:08:27 -0500

    connectance code uses the new interface

--- src/connectance.jl
+++ src/connectance.jl
@@ -1,26 +1,34 @@
 """ Expected number of links for a probabilistic matrix
 """
-function links(A::Array{Float64,2})
-   return sum(A)
+function links(N::EcoNetwork)
+   return sum(N.A)
 end
 
 """ Expected variance of the number of links for a probabilistic matrix
 """
-function links_var(A::Array{Float64,2})
-   return sum(A.*(1.-A))
+function links_var(N::UnipartiteProbaNetwork)
+   return sum(N.A.*(1.-N.A))
+end
+
+function links_var(N::BipartiteProbaNetwork)
+   return sum(N.A.*(1.-N.A))
 end
 
 """ Expected connectance for a probabilistic matrix, measured as the number
 of expected links, divided by the size of the matrix.
 """
-function connectance(A::Array{Float64,2})
-   return links(A) / prod(size(A))
+function connectance(N::EcoNetwork)
+   return links(N) / prod(size(N))
 end
 
 """ Expected variance of the connectance for a probabilistic matrix,
 measured as the variance of the number of links divided by the squared size of
 the matrix.
 """
-function connectance_var(A::Array{Float64,2})
-   return links_var(A) / (prod(size(A))^2)
+function connectance_var(N::BipartiteProbaNetwork)
+   return links_var(N) / (prod(size(N))^2)
+end
+
+function connectance_var(N::UnipartiteProbaNetwork)
+   return links_var(N) / (prod(size(N))^2)
 end
