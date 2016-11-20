e5dfd2b34d2b638de64f01ca418c52ce2c0f2715
Timothée Poisot <tim@poisotlab.io>
2016-11-20 17:43:30 -0500

    free species code updated for unipartite types

--- src/free_species.jl
+++ src/free_species.jl
@@ -1,37 +1,39 @@
-""" Probability that a species has no predecessors """
-function species_has_no_predecessors(A::Array{Float64,2})
-  @assert size(A)[1] == size(A)[2]
-  return vec(prod(1.0 .- nodiag(A),1))
+"""
+Probability that a species has no predecessors
+"""
+function species_has_no_predecessors(N::Unipartite)
+  return vec(prod(1.0 .- nodiag(N),1))
 end
 
-""" Probability that a species has no successors """
-function species_has_no_successors(A::Array{Float64,2})
-  @assert size(A)[1] == size(A)[2]
-  return vec(prod(1.0 .- nodiag(A),2))
+"""
+Probability that a species has no successors
+"""
+function species_has_no_successors(N::Unipartite)
+  return vec(prod(1.0 .- nodiag(N),2))
 end
 
-""" Probability that a species has no links
+"""
+Probability that a species has no links
 
 This will return a vector, where the *i*th element is the probability that
-species *i* has no interaction. Note that this is only meaningful for unipartite
-networks.
-
+species *i* has no interaction. Note that this is only meaningful for
+unipartite networks.
 """
-function species_is_free(A::Array{Float64,2})
-  @assert size(A)[1] == size(A)[2]
-  return species_has_no_successors(A) .* species_has_no_predecessors(A)
+function species_is_free(N::Unipartite)
+  return species_has_no_successors(N) .* species_has_no_predecessors(N)
 end
 
-""" Expected number of species with no interactions
+"""
+Expected number of species with no interactions
 
 This function will be applied on the *unipartite* version of the network. Note
-that the functions `species_ |predecessors` will work on bipartite networks, but
-the unipartite situation is more general.
-
+that the functions `species_ |predecessors` will work on bipartite networks,
+but the unipartite situation is more general.
 """
-function free_species(A::Array{Float64,2})
-  if length(unique(size(A))) == 1
-    return A |> species_is_free |> sum
-  end
-  return A |> make_unipartite |> species_is_free |> sum
+function free_species(N::EcoNetwork)
+    if N <: Unipartite
+        return N |> species_is_free |> sum
+    else
+        return N |> make_unipartite |> free_species
+    end
 end
