d6ca6437b2f51ad3fd4cd730b4e3862e53f7cb7e
Timothée Poisot <tim@poisotlab.io>
2016-11-20 17:30:37 -0500

    type testing updated to use bool

--- test/types.jl
+++ test/types.jl
@@ -10,12 +10,12 @@ module TestTypes
     @test typeof(UnipartiteProbaNetwork(rand(3, 3))) <: Unipartite
     @test typeof(UnipartiteProbaNetwork(rand(3, 3))) <: ProbabilisticNetwork
 
-    @test typeof(BipartiteNetwork(rand(Int64, (3, 3)))) == BipartiteNetwork
-    @test typeof(BipartiteNetwork(rand(Int64, (3, 3)))) <: Bipartite
-    @test typeof(BipartiteNetwork(rand(Int64, (3, 3)))) <: DeterministicNetwork
+    @test typeof(BipartiteNetwork(rand(Bool, (3, 3)))) == BipartiteNetwork
+    @test typeof(BipartiteNetwork(rand(Bool, (3, 3)))) <: Bipartite
+    @test typeof(BipartiteNetwork(rand(Bool, (3, 3)))) <: DeterministicNetwork
 
-    @test typeof(UnipartiteNetwork(rand(Int64, (3, 3)))) == UnipartiteNetwork
-    @test typeof(UnipartiteNetwork(rand(Int64, (3, 3)))) <: Unipartite
-    @test typeof(UnipartiteNetwork(rand(Int64, (3, 3)))) <: DeterministicNetwork
+    @test typeof(UnipartiteNetwork(rand(Bool, (3, 3)))) == UnipartiteNetwork
+    @test typeof(UnipartiteNetwork(rand(Bool, (3, 3)))) <: Unipartite
+    @test typeof(UnipartiteNetwork(rand(Bool, (3, 3)))) <: DeterministicNetwork
 
 end
