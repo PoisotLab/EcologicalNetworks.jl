08a89d731a9a040173d84e4cc7179add726d1ff8
Timothée Poisot <tim@poisotlab.io>
2016-11-20 16:23:19 -0500

    test type relationships

--- test/types.jl
+++ test/types.jl
@@ -6,4 +6,16 @@ module TestTypes
     @test typeof(BipartiteProbaNetwork(rand(3, 3))) <: Bipartite
     @test typeof(BipartiteProbaNetwork(rand(3, 3))) <: ProbabilisticNetwork
 
+    @test typeof(UnipartiteProbaNetwork(rand(3, 3))) == UnipartiteProbaNetwork
+    @test typeof(UnipartiteProbaNetwork(rand(3, 3))) <: Unipartite
+    @test typeof(UnipartiteProbaNetwork(rand(3, 3))) <: ProbabilisticNetwork
+
+    @test typeof(BipartiteNetwork(rand(3, 3))) == BipartiteNetwork
+    @test typeof(BipartiteNetwork(rand(3, 3))) <: Bipartite
+    @test typeof(BipartiteNetwork(rand(3, 3))) <: DeterministicNetwork
+
+    @test typeof(UnipartiteNetwork(rand(3, 3))) == UnipartiteNetwork
+    @test typeof(UnipartiteNetwork(rand(3, 3))) <: Unipartite
+    @test typeof(UnipartiteNetwork(rand(3, 3))) <: DeterministicNetwork
+
 end
