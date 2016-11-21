bc73fa7128d3d4be97fd2f4cbc36a76041e0a8c9
Timothée Poisot <tim@poisotlab.io>
2016-11-21 14:04:23 -0500

    modularity code update

--- test/modularity.jl
+++ test/modularity.jl
@@ -8,10 +8,11 @@ module TestModularity
          false false false true true true; false false false true true true]
     B = BipartiteNetwork(A)
     U = make_unipartite(B)
-    L = collect(1:richness(B))
-    mb = label_propagation(B, L)
-    mu = label_propagation(U, L)
+    mb = label_propagation(B, collect(1:richness(B)))
+    mu = label_propagation(U, collect(1:richness(U)))
 
+    @test mb.Q == 0.5
+    @test length(unique(mb.L)) == 2
     @test mb.Q == mu.Q
     @test Qr(mb) == 1.0
 
