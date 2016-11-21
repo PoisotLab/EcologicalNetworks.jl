dab9920e5c9db2fc8ac581ab491bb8dfcde408ba
Timothée Poisot <tim@poisotlab.io>
2016-11-20 20:30:36 -0500

    fix texts for the null model wrapper

--- test/nullmodelswrapper.jl
+++ test/nullmodelswrapper.jl
@@ -3,20 +3,20 @@ module TestNullModelsWrapper
     using EcologicalNetwork
 
     # Test 1 -- model with one free species = no output
-    A = map(Bool, [0.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0])
-    N = UnipartiteNetwork(A)
+    A = [0.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
+    N = UnipartiteProbaNetwork(A)
     B = nullmodel(N, n=10, max=10)
     @test length(B) == 0
 
     # Test 2 -- full graph converges
-    A = map(Bool, [1.0 1.0; 1.0 1.0])
-    N = UnipartiteNetwork(A)
+    A = [1.0 1.0; 1.0 1.0]
+    N = UnipartiteProbaNetwork(A)
     B = nullmodel(N, n=10, max=10)
     @test length(B) == 10
 
     # Test 3 -- works if max < n
-    A = map(Bool, [1.0 1.0; 1.0 1.0])
-    N = UnipartiteNetwork(A)
+    A = [1.0 1.0; 1.0 1.0]
+    N = UnipartiteProbaNetwork(A)
     B = nullmodel(N, n=5, max=2)
     @test length(B) == 5
 
