80f30f33c7e19fac7a9476f0bb2326d7072afce7
Timothée Poisot <tim@poisotlab.io>
2016-11-20 21:08:11 -0500

    fix centrality

--- src/centrality.jl
+++ src/centrality.jl
@@ -7,10 +7,9 @@ Measures Katz's centrality for each node in a unipartite network.
 - `k` (def. 5), the maximal path length considered
 """
 function centrality_katz(N::Unipartite; a::Float64=0.1, k::Int64=5)
-	@assert size(A)[1] == size(A)[2]
 	@assert a <= 1.0
 	@assert a >= 0.0
 	@assert k >= 1
-	centr = sum(hcat(map((x) -> vec(sum((a^x).*(A^x),1)), [1:k;])...),2)
+	centr = sum(hcat(map((x) -> vec(sum((a^x).*(N.A^x),1)), [1:k;])...),2)
 	return centr ./ sum(centr)
 end
