5699b332881fe8f33a5ee91ea463c5df1be4ff24
Timothée Poisot <tim@poisotlab.io>
2016-11-23 11:41:00 -0500

    prepare doc for deploy

--- docs/make.jl
+++ docs/make.jl
@@ -6,3 +6,11 @@ push!(LOAD_PATH, "../src/")
 makedocs(
          modules = [EcologicalNetwork]
         )
+
+deploydocs(
+           deps   = Deps.pip("mkdocs", "python-markdown-math"),
+           repo   = "github.com/PoisotLab/EcologicalNetwork.jl.git",
+           julia  = "0.5",
+           osname = "linux"
+          )
+
