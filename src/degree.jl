bfca49c4d29b27e2c164ee0f078a3d066f4e60c6
Timothée Poisot <tim@poisotlab.io>
2016-11-23 11:04:10 -0500

    add a degree function for bipartite networks, make them unipartite first

--- src/degree.jl
+++ src/degree.jl
@@ -18,6 +18,10 @@ function degree(N::Unipartite)
   return degree_in(N) .+ degree_out(N)
 end
 
+function degree(N::Bipartite)
+    return degree(make_unipartite(N))
+end
+
 function degree_out_var(N::ProbabilisticNetwork)
   return mapslices(a_var, N.A, 2)
 end
