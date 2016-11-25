55cf65bda18b5194bf23c8f4d6ce840448ae234e
Timothée Poisot <tim@poisotlab.io>
2016-11-25 14:28:28 -0500

    network files

--- src/data.jl
+++ src/data.jl
@@ -4,7 +4,8 @@ Stony food web from Thompson & Townsend
 Taken from `https://www.nceas.ucsb.edu/interactionweb/html/thomps_towns.html`
 """
 function stony()
-    return UnipartiteNetwork(readdlm("data/du_stony.txt"))
+    n_path = joinpath(Pkg.dir("EcologicalNetwork"), "data", "du_stony.txt")
+    return UnipartiteNetwork(readdlm(n_path))
 end
 
 """
@@ -13,6 +14,7 @@ Plant-flower visitor interactions in the Galapagos.
 Taken from `https://www.nceas.ucsb.edu/interactionweb/html/mc_mullen.html`
 """
 function mcmullen()
-    return BipartiteNetwork(readdlm("data/db_mcmullen.txt"))
+    n_path = joinpath(Pkg.dir("EcologicalNetwork"), "data", "db_mcmullen.txt")
+    return BipartiteNetwork(readdlm(n_path))
 end
 
