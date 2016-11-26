296efaa4b3aae0f075aec2d73edf6b96e9da6e3a
Timothée Poisot <tim@poisotlab.io>
2016-11-25 21:22:59 -0500

    data tests

--- test/data.jl
+++ test/data.jl
@@ -15,4 +15,14 @@ module TestData
     @test typeof(ollerton()) <: DeterministicNetwork
     @test size(ollerton()) == (26, 10)
 
+    # Bluthgen
+    @test typeof(bluthgen()) <: Bipartite
+    @test typeof(bluthgen()) <: QuantitativeNetwork
+    @test sizeof(bluthgen()) == (41, 51)
+
+    # Robertson
+    @test typeof(robertson()) <: Bipartite
+    @test typeof(robertson()) <: DeterministicNetwork
+    @test size(robertson()) == (1429, 456)
+
 end
