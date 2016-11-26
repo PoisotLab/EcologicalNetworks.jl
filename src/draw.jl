1b04f5467dca779aa242dfac88d9a0827952732e
Timothée Poisot <tim@poisotlab.io>
2016-11-25 22:03:57 -0500

    draw from a re-ordered network #43

--- src/draw.jl
+++ src/draw.jl
@@ -1,8 +1,41 @@
-function draw_matrix(A::Array{Float64,2}; transform = (x) -> 1-x, file="pen.png")
+"""
+Draw a network as a matrix to a file
+"""
+function plot_network(N::EcoNetwork; order::Symbol=:degree, transform::Function=(x) -> x, file="en.png")
+    @assert order ∈ [:degree, :none]
+    
+    # Convert to floating point values
+    A = map(Float64, N.A)
+
+    # Ranges the matrix in 0-1
+    A = A ./ maximum(A)
+
+    # Apply the transformation if needed
+    for i in eachindex(A)
+        A[i] = A[i] == 0.0 ? 0.0 : transform(0.0)
+    end
+
+    # If we re-order the species by degree...
+    if typeof(N) <: Unipartite
+        ord = sortperm(degree(N))
+        A = A[ord, ord]
+    else
+        ord_row = sortperm(degree_out(N))
+        ord_col = sortperm(degree_in(N))
+        A = A[ord_row, ord_col]
+    end
+
+
+end
+
+"""
+Low-level function to draw the network
+"""
+function draw_matrix(A::Array{Float64,2}; file="ecologicalnetwork.png")
     nbot, ntop = size(A)
     # Check size
-    @assert nbot <= 1000
-    @assert ntop <= 1000
+    @assert nbot <= 4000
+    @assert ntop <= 4000
     # Get image size
     width  = 4 + nbot*(10+4)
     height = 4 + ntop*(10+4)
