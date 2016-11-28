13d76c727ba2c1aa3aed6a4bf00268d146a40d15
Timothée Poisot <tim@poisotlab.io>
2016-11-28 15:56:30 -0500

    update tests with wnodf example values

--- test/nestedness.jl
+++ test/nestedness.jl
@@ -20,6 +20,23 @@ module TestNestedness
     @test_approx_eq_eps nodf(AN)[2] 0.63 0.01
     @test_approx_eq_eps nodf(AN)[3] 0.53 0.01
 
+    # Some WNODF examples
+    WN1A = BipartiteQuantiNetwork([5 4 3 2 1; 4 3 2 1 0; 3 2 1 0 0; 2 1 0 0 0; 1 0 0 0 0])
+    WN1B = BipartiteQuantiNetwork([5 4 3 2 1; 5 4 3 2 0; 5 4 3 0 0; 5 4 0 0 0; 5 0 0 0 0])
+    WN1D = BipartiteQuantiNetwork([5 1 1 1 1; 1 0 0 0 0; 1 0 0 0 0; 1 0 0 0 0; 1 0 0 0 0])
+
+    @test_approx_eq_eps nodf(WN1A)[1] 1.0 0.01
+    @test_approx_eq_eps nodf(WN1A)[2] 1.0 0.01
+    @test_approx_eq_eps nodf(WN1A)[3] 1.0 0.01
+
+    @test_approx_eq_eps nodf(WN1B)[1] 0.5 0.01
+    @test_approx_eq_eps nodf(WN1B)[2] 1.0 0.01
+    @test_approx_eq_eps nodf(WN1B)[3] 0.0 0.01
+
+    @test_approx_eq_eps nodf(WN1D)[1] 0.4 0.01
+    @test_approx_eq_eps nodf(WN1D)[2] 0.4 0.01
+    @test_approx_eq_eps nodf(WN1D)[3] 0.4 0.01
+
 
 
 end
