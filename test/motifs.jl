557d470816e46fde770f35636c6ecb5e47aaa604
Timothée Poisot <tim@poisotlab.io>
2016-11-29 12:24:44 -0500

    use testsets for some ut

--- test/motifs.jl
+++ test/motifs.jl
@@ -1,41 +1,71 @@
 module TestMotifs
-  using Base.Test
-  using EcologicalNetwork
+    using Base.Test
+    using EcologicalNetwork
 
-  # Test with a single link
-  N = UnipartiteProbaNetwork([0.2 0.8; 0.2 0.1])
-  m = UnipartiteNetwork([false true; false false])
-  pmotif = EcologicalNetwork.motif_internal(N, m)
-  @test pmotif == vec([0.8 0.8 0.8 0.9])
+    @testset "Single link, probabilistic" begin
+        # Test with a single link
+        N = UnipartiteProbaNetwork([0.2 0.8; 0.2 0.1])
+        m = UnipartiteNetwork([false true; false false])
+        pmotif = EcologicalNetwork.motif_internal(N, m)
+        @test pmotif == vec([0.8 0.8 0.8 0.9])
+    end
 
-  # Test with a perfect match
-  N = UnipartiteProbaNetwork([0.0 1.0; 0.0 0.0])
-  m = UnipartiteNetwork([false true; false false])
-  pmotif = EcologicalNetwork.motif_internal(N, m)
-  @test pmotif == vec([1.0 1.0 1.0 1.0])
-  @test motif_p(N, m) == 1.0
-  @test motif_v(N, m) == 0.0
+    @testset "Perfect match, probabilistic" begin
+        # Test with a perfect match
+        N = UnipartiteProbaNetwork([0.0 1.0; 0.0 0.0])
+        m = UnipartiteNetwork([false true; false false])
+        pmotif = EcologicalNetwork.motif_internal(N, m)
+        @test pmotif == vec([1.0 1.0 1.0 1.0])
+        @test motif_p(N, m) == 1.0
+        @test motif_v(N, m) == 0.0
+    end
 
-  # Test on a three-species network
-  B = UnipartiteNetwork([false true true; false false true; false false false])
-  @test motif(B, B) == 1.0
-  
-  BDN = BipartiteNetwork([false true true; false false true; false false false])
-  @test motif(BDN, BDN) == 1.0
+    @testset "Fork food web" begin
+        # Test with a fork-like thing
+        diam = UnipartiteNetwork([0 1 0 0; 0 0 1 1; 0 0 0 0; 0 0 0 0])
+        clin = UnipartiteNetwork([0 1 0; 0 0 1; 0 0 0])
+        capp = UnipartiteNetwork([0 1 1; 0 0 0; 0 0 0])
+        cdir = UnipartiteNetwork([0 0 1; 0 0 1; 0 0 0])
 
-  # Test on the same network, this time with a proba one
-  P = UnipartiteProbaNetwork([0.0 1.0 1.0; 0.0 0.0 1.0; 0.0 0.0 0.0])
-  @test motif_var(P, B) == 0.0
+        @test motif(diam, clin) ≈ 2.0
+        @test motif(diam, capp) ≈ 1.0
+        @test motif(diam, cdir) ≈ 0.0
+    end
 
-  # Test with some variation
-  ovl = UnipartiteNetwork([false true true; false false true; false false false])
-  N = UnipartiteProbaNetwork([0.0 1.0 0.8; 0.0 0.0 1.0; 0.0 0.0 0.0])
-  @test_approx_eq motif(N, ovl) 0.8
-  fchain = UnipartiteNetwork([false true false; false false true; false false false])
-  @test_approx_eq motif(N, fchain) 0.2
+    @testset "Diamond food web" begin
+        # Test with a diamond food web
+        diam = UnipartiteNetwork([0 1 1 0; 0 0 0 1; 0 0 0 1; 0 0 0 0])
+        clin = UnipartiteNetwork([0 1 0; 0 0 1; 0 0 0])
+        capp = UnipartiteNetwork([0 1 1; 0 0 0; 0 0 0])
+        cdir = UnipartiteNetwork([0 0 1; 0 0 1; 0 0 0])
 
-  # Test of the simplest situation: two nodes, ten random matrices
-  for i in 1:10
+        @test motif(diam, clin) ≈ 2.0
+        @test motif(diam, capp) ≈ 1.0
+        @test motif(diam, cdir) ≈ 1.0
+    end
+
+    @testset "Three species" begin
+        # Test on a three-species network
+        B = UnipartiteNetwork([false true true; false false true; false false false])
+        @test motif(B, B) == 1.0
+    end
+
+    BDN = BipartiteNetwork([false true true; false false true; false false false])
+    @test motif(BDN, BDN) == 1.0
+
+    # Test on the same network, this time with a proba one
+    P = UnipartiteProbaNetwork([0.0 1.0 1.0; 0.0 0.0 1.0; 0.0 0.0 0.0])
+    @test motif_var(P, B) == 0.0
+
+    # Test with some variation
+    ovl = UnipartiteNetwork([false true true; false false true; false false false])
+    N = UnipartiteProbaNetwork([0.0 1.0 0.8; 0.0 0.0 1.0; 0.0 0.0 0.0])
+    @test_approx_eq motif(N, ovl) 0.8
+    fchain = UnipartiteNetwork([false true false; false false true; false false false])
+    @test_approx_eq motif(N, fchain) 0.2
+
+    # Test of the simplest situation: two nodes, ten random matrices
+    for i in 1:10
     N = BipartiteProbaNetwork(rand((2, 2)))
     possible_motifs = (
         [0 1; 0 0], [1 0; 0 0], [0 0; 1 0], [0 0; 0 1],
@@ -47,6 +77,6 @@ module TestMotifs
     )
     all_probas = map((x) -> motif_p(N, BipartiteNetwork(map(Bool, x))), possible_motifs)
     @test_approx_eq sum(all_probas) 1.0
-  end
+    end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -27,10 +27,10 @@ my_tests = [
 for my_test in my_tests
   try
     include(my_test)
-    println("\t\033[1m\033[32mPASSED\033[0m: $(my_test)")
+    println(">> \033[1m\033[32m++\033[0m $(my_test)")
   catch e
     anyerrors = true
-    println("\t\033[1m\033[31mFAILED\033[0m: $(my_test)")
+    println(">> \033[1m\033[31m!!\033[0m $(my_test)")
     showerror(STDOUT, e, backtrace())
     println()
   end
