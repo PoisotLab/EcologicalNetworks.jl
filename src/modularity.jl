4cbb1180ea94b027801301f74e3354fb9094954e
Timothée Poisot <tim@poisotlab.io>
2016-11-29 11:20:28 -0500

    fix modularity

--- src/modularity.jl
+++ src/modularity.jl
@@ -355,6 +355,6 @@ function best_partition(modpart; f::Function=Q)
     best_pos = collect(1:length(crit))[crit .== maximum(crit)]
 
     # Then return the best partitions
-    return best_pos
+    return M[best_pos]
 
 end
