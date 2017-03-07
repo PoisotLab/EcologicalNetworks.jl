6b04420223832eb3f7b899186fff165e1da91b05
Timothée Poisot <timothee.poisot@umontreal.ca>
2017-03-07 15:54:45 -0500

    fix indentation

--- src/data.jl
+++ src/data.jl
@@ -4,29 +4,29 @@ near Otago, New Zealand. Note that there is, in the original matrix, a species
 with no interactions. It is removed when generating the network.
 """
 function stony()
-    n_path = joinpath(Pkg.dir("EcologicalNetwork"), "data", "du_stony.txt")
-    N = UnipartiteNetwork(readdlm(n_path))
-    # remove species without interactions
-    have_deg = degree(N) .> 0
-    keep_sp = filter(x -> have_deg[x], 1:richness(N))
-    # return
-    return UnipartiteNetwork(N[keep_sp, keep_sp])
+  n_path = joinpath(Pkg.dir("EcologicalNetwork"), "data", "du_stony.txt")
+  N = UnipartiteNetwork(readdlm(n_path))
+  # remove species without interactions
+  have_deg = degree(N) .> 0
+  keep_sp = filter(x -> have_deg[x], 1:richness(N))
+  # return
+  return UnipartiteNetwork(N[keep_sp, keep_sp])
 end
 
 """
 Plant-flower visitor interactions in the Galapagos.
 """
 function mcmullen()
-    n_path = joinpath(Pkg.dir("EcologicalNetwork"), "data", "db_mcmullen.txt")
-    return BipartiteNetwork(readdlm(n_path))
+  n_path = joinpath(Pkg.dir("EcologicalNetwork"), "data", "db_mcmullen.txt")
+  return BipartiteNetwork(readdlm(n_path))
 end
 
 """
 Fish-anemone interactions from Ollerton et al. 2007
 """
 function ollerton()
-    n_path = joinpath(Pkg.dir("EcologicalNetwork"), "data", "db_ollerton.txt")
-    return BipartiteNetwork(readdlm(n_path))
+  n_path = joinpath(Pkg.dir("EcologicalNetwork"), "data", "db_ollerton.txt")
+  return BipartiteNetwork(readdlm(n_path))
 end
 
 """
@@ -34,25 +34,25 @@ Pollination interaction from Robertson 1927, in an agricultural area dominated
 by crops, with some natural forest and pasture.
 """
 function robertson()
-    n_path = joinpath(Pkg.dir("EcologicalNetwork"), "data", "db_robertson.txt")
-    return BipartiteNetwork(readdlm(n_path))
+  n_path = joinpath(Pkg.dir("EcologicalNetwork"), "data", "db_robertson.txt")
+  return BipartiteNetwork(readdlm(n_path))
 end
 
 """
 Number of visits from Bluthgen et al XXX
 """
 function bluthgen()
-    n_path = joinpath(Pkg.dir("EcologicalNetwork"), "data", "qb_bluthgen.txt")
-    return BipartiteQuantiNetwork(readdlm(n_path))
+  n_path = joinpath(Pkg.dir("EcologicalNetwork"), "data", "qb_bluthgen.txt")
+  return BipartiteQuantiNetwork(readdlm(n_path))
 end
 
 """
 Lake of the Woods host-parasite data. Interactions content are prevalence.
 """
 function woods()
-    n_path = joinpath(Pkg.dir("EcologicalNetwork"), "data", "qb_woods.txt")
-    # This matrix is in the wrong format on IWDB
-    return BipartiteQuantiNetwork(readdlm(n_path)')
+  n_path = joinpath(Pkg.dir("EcologicalNetwork"), "data", "qb_woods.txt")
+  # This matrix is in the wrong format on IWDB
+  return BipartiteQuantiNetwork(readdlm(n_path)')
 end
 
 """
