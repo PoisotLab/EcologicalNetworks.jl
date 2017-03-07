1a5f030f95c3fed19b20ef7ddf9d6aceb0f2f128
Timothée Poisot <tim@poisotlab.io>
2017-03-06 22:20:53 -0500

    update indentation in main file

--- src/EcologicalNetwork.jl
+++ src/EcologicalNetwork.jl
@@ -7,75 +7,75 @@ using Combinatorics
 
 export EcoNetwork,
 
-    # General types for all bipartite / unipartite
-    Bipartite, Unipartite,
+# General types for all bipartite / unipartite
+Bipartite, Unipartite,
 
-    # Types
-    BipartiteNetwork, UnipartiteNetwork,
-    BipartiteProbaNetwork, UnipartiteProbaNetwork,
-    BipartiteQuantiNetwork, UnipartiteQuantiNetwork,
+# Types
+BipartiteNetwork, UnipartiteNetwork,
+BipartiteProbaNetwork, UnipartiteProbaNetwork,
+BipartiteQuantiNetwork, UnipartiteQuantiNetwork,
 
-    # Union types for all proba or deterministic
-    ProbabilisticNetwork, DeterministicNetwork, QuantitativeNetwork,
-    NonProbabilisticNetwork,
+# Union types for all proba or deterministic
+ProbabilisticNetwork, DeterministicNetwork, QuantitativeNetwork,
+NonProbabilisticNetwork,
 
-    # Richness
-    richness,
+# Richness
+richness,
 
-    # Nestedness
-    η, nodf,
+# Nestedness
+η, nodf,
 
-    # Measures of centrality
-    centrality_katz,
+# Measures of centrality
+centrality_katz,
 
-    # Links and connectances
-    links, links_var, connectance, connectance_var,
-    linkage_density, link_number,
+# Links and connectances
+links, links_var, connectance, connectance_var,
+linkage_density, link_number,
 
-    # Measures of degree
-    degree_out, degree_in, degree_out_var, degree_in_var, degree, degree_var,
-    specificity,
+# Measures of degree
+degree_out, degree_in, degree_out_var, degree_in_var, degree, degree_var,
+specificity,
 
-    # Expected number of species with 0 interactions
-    species_has_no_successors, species_has_no_predecessors,
-    species_is_free, free_species,
+# Expected number of species with 0 interactions
+species_has_no_successors, species_has_no_predecessors,
+species_is_free, free_species,
 
-    # Matrix manipulation utilities
-    make_unipartite, make_threshold, make_binary, make_bernoulli, nodiag,
-    adjacency,
+# Matrix manipulation utilities
+make_unipartite, make_threshold, make_binary, make_bernoulli, nodiag,
+adjacency,
 
-    # Probability algebra utilities
-    checkprob, i_esp, i_var, a_var, m_var,
+# Probability algebra utilities
+checkprob, i_esp, i_var, a_var, m_var,
 
-    # Null models
-    null1, null2, null3out, null3in, nullmodel,
+# Null models
+null1, null2, null3out, null3in, nullmodel,
 
-    # Swap
-    swap,
+# Swap
+swap,
 
-    # Testing
-    test_network_property,
+# Testing
+test_network_property,
 
-    # Modularity
-    Partition, Q, Qr, label_propagation, modularity, best_partition,
+# Modularity
+Partition, Q, Qr, label_propagation, modularity, best_partition,
 
-    # Draw
-    plot_network,
+# Draw
+plot_network,
 
-    # Paths
-    number_of_paths,
+# Paths
+number_of_paths,
 
-    # Beta-diversity
-    betadiversity,
-    whittaker, sorensen, jaccard, gaston,
-    williams, lande, ruggiero, hartekinzig,
-    harrison,
+# Beta-diversity
+betadiversity,
+whittaker, sorensen, jaccard, gaston,
+williams, lande, ruggiero, hartekinzig,
+harrison,
 
-    # Motifs
-    motif_p, motif_v, count_motifs, motif, motif_var, unipartitemotifs,
+# Motifs
+motif_p, motif_v, count_motifs, motif, motif_var, unipartitemotifs,
 
-    # Data
-    stony, mcmullen, ollerton, bluthgen, robertson, woods
+# Data
+stony, mcmullen, ollerton, bluthgen, robertson, woods
 
 include(joinpath(".", "types.jl"))
 include(joinpath(".", "centrality.jl"))
