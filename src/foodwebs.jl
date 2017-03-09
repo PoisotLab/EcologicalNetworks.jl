1283fb37afce778523a50f94e03e1477d4bc0caa
Timothée Poisot <timothee.poisot@umontreal.ca>
2017-03-09 11:15:51 -0500

    :books: latex syntax in docstrings

--- src/foodwebs.jl
+++ src/foodwebs.jl
@@ -13,7 +13,7 @@ as its input. There is currently no trophic rank formulation for probabilistic
 networks.
 
 The primary producers have a rank of 1, and species consuming any species of
-maximal level $n$ have level $n+1$.
+maximal level ``n`` have level ``n+1``.
 
 Note that the trophic level is measured on the network with self-interactions
 removed.
@@ -60,13 +60,13 @@ This function weighs the fractional trophic level as returned by
 `fractional_trophic_level` by the proportion of the prey in the predator's diet.
 Specifically, this is done with:
 
-$\text{TL}_i = 1 + \sum_{j \in \text{preys}} \left(\mathbf{f}_j\times\mathbf{D}_{ij}\right)$
+``\text{TL}_i = 1 + \sum_{j \in \text{preys}} \left(\mathbf{f}_j\times\mathbf{D}_{ij}\right)``
 
-The $\mathbf{j}$ array has the fractional trophic levels, and $\mathbf{D}$ is
-the matrix with diet proportions. $\mathbf{D}_{ij}$ is $0.0$ for all
+The ``\mathbf{j}`` array has the fractional trophic levels, and ``\mathbf{D}``
+is the matrix with diet proportions. ``\mathbf{D}_{ij}`` is ``0.0`` for all
 non-consumed preys. In a quantitative network, it is
-$\mathbf{A}_{ij}/\sum\mathbf{A}_{i\dot}$, and it is the same in deterministic
-networks although it works out to $1 / k_{o}(i)$ in the end.
+``\mathbf{A}_{ij}/\sum\mathbf{A}_{i\dot}``, and it is the same in deterministic
+networks although it works out to ``1 / k_{o}(i)`` in the end.
 
 As for `fractional_trophic_level`, this function is applied to the network
 without self-edges.
