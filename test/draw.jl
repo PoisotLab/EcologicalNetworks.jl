381a92e7d025e6efa53febe1d3b044ec5cb43c22
Timothée Poisot <timothee.poisot@umontreal.ca>
2017-07-07 13:51:50 -0400

    :rotating_light: drawing tests 2

--- test/draw.jl
+++ test/draw.jl
@@ -8,7 +8,7 @@ n1 = BipartiteNetwork(rand(Bool, (1, 19)))
 n2 = BipartiteNetwork(rand(Bool, (19, 1)))
 p = label_propagation(n, collect(1:20))
 
-for t in [n1, n2, n3]
+for t in [n1, n2, n]
   plot(t)
   plot(t, p)
 end
