b4904413b64ac130f39acabab4e4e938ef389388
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-03-08 16:14:57 -0500

    :white_check_mark: add quantitative shortest path

--- test/paths.jl
+++ test/paths.jl
@@ -17,4 +17,9 @@ module TestPaths
   d = shortest_path(u[:S1], nmax=200)
   @test d == [0 1 2; 0 0 1; 0 0 0]
 
+  # Shortest path on a quanti network
+  A = UnipartiteQuantiNetwork([0.0 0.2 0.0; 0.0 0.0 0.4; 0.0 0.0 0.0])
+  @test shortest_path(A) == shortest_path(u[:S1], nmax=200)
+
+
 end
