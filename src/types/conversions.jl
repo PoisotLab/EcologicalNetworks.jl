740d20a1dd8efe4521ba50a28e4dc6ab12f3fbc2
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-04-09 13:09:00 -0400

    :hand: convert to abstract unipartite in a general way

--- src/types/conversions.jl
+++ src/types/conversions.jl
@@ -36,3 +36,15 @@ function convert{NT,IT}(::Type{BipartiteNetwork}, N::BipartiteQuantitativeNetwor
     C = N.A.>zero(eltype(N.A))
     return BipartiteNetwork(convert(Array{Bool,2}, C), T, B)
 end
+
+function convert(::Type{AbstractUnipartiteNetwork}, N::AbstractBipartiteNetwork)
+    if typeof(N) <: QuantitativeNetwork
+        return convert(UnipartiteQuantitativeNetwork, N)
+    end
+    if typeof(N) <: BinaryNetwork
+        return convert(UnipartiteNetwork, N)
+    end
+    if typeof(N) <: ProbabilisticNetwork
+        return convert(UnipartiteProbabilisticNetwork, N)
+    end
+end
--- test/types/conversions.jl
+++ test/types/conversions.jl
@@ -26,6 +26,10 @@ M = convert(UnipartiteProbabilisticNetwork, N)
 @test has_interaction(M, :b, :A)
 @test has_interaction(M, :b, :B)
 @test !has_interaction(M, :A, :B)
+L = convert(AbstractUnipartiteNetwork, N)
+@test has_interaction(L, :a, :A)
+@test typeof(L) <: AbstractUnipartiteNetwork
+@test typeof(L) <: UnipartiteProbabilisticNetwork
 
 N = BipartiteQuantitativeNetwork([4 0 ; 2 1], [:a, :b], [:A, :B])
 M = convert(BipartiteNetwork, N)
