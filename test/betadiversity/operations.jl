2d65ad8796fa882f8f35fb21c69e84ef39efba24
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-04-25 15:36:17 -0400

    :hand: test beta div

--- test/betadiversity/operations.jl
+++ test/betadiversity/operations.jl
@@ -16,10 +16,14 @@ module TestBetaDivOperations
   A = BipartiteNetwork(rand(Bool, (10, 10)))
   B = BipartiteNetwork(rand(Bool, (20, 20)))
   U = union(A,B)
+  I = intersect(A,B)
   @assert typeof(U) == typeof(B)
   @assert links(U) >= links(A)
   @assert links(U) >= links(B)
   @assert richness(U) == richness(B)
+  @assert links(I) <= links(B)
+  @assert links(I) <= links(A)
+  @assert richness(I) == richness(B)
 
   # TODO no setdiff
   # TODO only unique species
