7919d8b895ac1e62380704b7f6578dcb9708f3d9
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-04-28 18:25:37 -0400

    :hand: misc

--- test.jl
+++ test.jl
@@ -3,7 +3,7 @@ using EcologicalNetwork
 
 
 
-N = simplify(nodiagonal(nz_stream_foodweb()[12]))
+N = simplify(nodiagonal(nz_stream_foodweb()[1]))
 l = foodweb_layout(N; steps=100)
 l = foodweb_layout(l...; steps=100)
 graph_network_plot(l...; names=false)
