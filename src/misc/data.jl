950bf9e7fb4811439386bd3e80c5cddc293b0c17
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-05-14 13:59:36 -0400

    Add tests for all WOL networks

--- src/misc/data.jl
+++ src/misc/data.jl
@@ -20,19 +20,20 @@ function web_of_life(name)
   content = readdlm(joinpath(@__DIR__, "../..", "data", "weboflife", fullname), ',')
   bottom_species_names = convert(Array{String}, vec(content[:,1][2:end]))
   top_species_names = convert(Array{String}, vec(content[1,:][2:end]))
-  interaction_matrix = map(Int64, content[2:end,2:end])'
+  int_mat = content[2:end,2:end]
+  repl = find(int_mat .== "#")
+  int_mat[repl] = 0.0
+  interaction_matrix = map(Int64, round.(int_mat, 0))'
   ntype = BipartiteQuantitativeNetwork
   if maximum(interaction_matrix) == 1
     interaction_matrix = interaction_matrix .> 0
     ntype = BipartiteNetwork
   end
   this_net = ntype(interaction_matrix, top_species_names, bottom_species_names)
-  if "Num. of hosts sampled" ∈ species(this_net, 1)
-    sp_1 = filter(x -> x != "Num. of hosts sampled", species(this_net,1))
-    return this_net[sp_1,species(this_net, 2)]
-  else
-    return this_net
-  end
+  forbidden_names = ["Numbers of flowers", "Num. of hosts sampled", "#", "Frequency of occurrences", "Number of flowers", "Number of droppings analysed"]
+  sp_1 = filter(x -> !(x ∈ forbidden_names), species(this_net,1))
+  sp_2 = filter(x -> !(x ∈ forbidden_names), species(this_net,2))
+  return this_net[sp_1, sp_2]
 end
 
 function web_of_life()
--- test/data.jl
+++ test/data.jl
@@ -2,22 +2,18 @@ module TestData
   using Base.Test
   using EcologicalNetwork
 
-  # Catlins
-  #ttc = thompson_townsend_catlins()
-  #@test typeof(ttc) <: UnipartiteNetwork
-  #@test richness(ttc) == 49
-  #@test degree_out(ttc)["Unidentified detritus"] == 0
+  all_wol = web_of_life()
+  for wol in all_wol
+    N = web_of_life(wol.ID)
+    @test richness(N) == wol.Species
+    @test links(N) == wol.Interactions
+  end
 
-  #fc96 = fonseca_ganade_1996()
-  #@test typeof(fc96) <: AbstractBipartiteNetwork
-  #@test typeof(fc96) <: QuantitativeNetwork
-  #@test typeof(fc96) <: BipartiteQuantitativeNetwork
-  #@test richness(fc96) == 16+25
+  for n in nz_stream_foodweb()
+    @test typeof(n) <: BinaryNetwork
+    @test typeof(n) <: AbstractUnipartiteNetwork
+  end
 
-  #mc93 = mccullen_1993()
-  #@test typeof(mc93) <: AbstractBipartiteNetwork
-  #@test richness(mc93) == 159
-  #@test richness(mc93,1) == 54
-  #@test richness(mc93,2) == 105
+
 
 end
