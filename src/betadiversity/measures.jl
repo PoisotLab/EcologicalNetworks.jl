88e21d85c911794e4c01c8d0716fdc79974e3d55
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-05-16 14:12:18 -0400

    :hammer: betadiversity type declaration

--- src/betadiversity/measures.jl
+++ src/betadiversity/measures.jl
@@ -1,44 +1,44 @@
 """ Whittaker measure of beta-diversity """
-function whittaker{T<:NamedTuple}(S::T)
+function whittaker(S::T) where {T<:NamedTuple}
   return (S.a+S.b+S.c)/((S.a + (S.a+S.b+S.c))/2.0) - 1.0
 end
 
 """ Sorensen measure of beta-diversity """
-function sorensen{T<:NamedTuple}(S::T)
+function sorensen(S::T) where {T<:NamedTuple}
   return (2.0*S.a)/(S.a + (S.a+S.b+S.c))
 end
 
 """ Jaccard measure of beta-diversity """
-function jaccard{T<:NamedTuple}(S::T)
+function jaccard(S::T) where {T<:NamedTuple}
   return S.a/(S.a+S.b+S.c)
 end
 
 """ Gaston measure of beta-diversity """
-function gaston{T<:NamedTuple}(S::T)
+function gaston(S::T) where {T<:NamedTuple}
   return (S.b+S.c)/(S.a+S.b+S.c)
 end
 
 """ Williams measure of beta-diversity """
-function williams{T<:NamedTuple}(S::T)
+function williams(S::T) where {T<:NamedTuple}
   return minimum(vec([S.b S.c]))/(S.a+S.b+S.c)
 end
 
 """ Lande measure of beta-diversity """
-function lande{T<:NamedTuple}(S::T)
+function lande(S::T) where {T<:NamedTuple}
   return (S.b + S.c)/2.0
 end
 
 """ Ruggiero measure of beta-diversity """
-function ruggiero{T<:NamedTuple}(S::T)
+function ruggiero(S::T) where {T<:NamedTuple}
   return (S.a)/(S.a + S.c)
 end
 
 """ Harte-Kinzig measure of beta-diversity """
-function hartekinzig{T<:NamedTuple}(S::T)
+function hartekinzig(S::T) where {T<:NamedTuple}
   return 1.0 - (2.0 * S.a)/(S.a + (S.a+S.b+S.c))
 end
 
 """ Harrison measure of beta-diversity """
-function harrison{T<:NamedTuple}(S::T)
+function harrison(S::T) where {T<:NamedTuple}
   return minimum(vec([S.b S.c]))/(minimum(vec([S.b S.c])) + S.a)
 end
