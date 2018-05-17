ae57611c40234c25868a9d14fa316931aed6178e
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-05-17 10:36:07 -0400

    :wrench: test to generate networks from abundances

--- test.jl
+++ test.jl
@@ -2,10 +2,25 @@ include("./src/EcologicalNetwork.jl")
 using EcologicalNetwork
 using Base.Test
 
-N = first(nz_stream_foodweb())
+using Distributions
 
-import Base: eltype
+d_host = LogNormal(2.0, 2.0)
+d_parasite = LogNormal(2.2, 1.4)
+
+N_host = sort(rand(d_host, 20))
+N_parasite = sort(rand(d_parasite, 10))
+
+N_host = N_host ./ sum(N_host)
+N_parasite = N_parasite ./ sum(N_parasite)
+
+NP = N_parasite.*N_host'
 
 
+using StatsBase
+n_int = sample(collect(eachindex(NP)), weights(vec(NP)), 2000)
+A = zeros(Int64, NP)
+for i in n_int
+    A[i] += 1
+end
 
-eltype(web_of_life("A_HP_001"))
+N = BipartiteQuantitativeNetwork(A)
