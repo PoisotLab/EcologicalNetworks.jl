5fc84553292a675a2b402ec4f8f4ba043058ab3b
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-05-17 16:10:24 -0400

    :memo: documentation config

--- docs/make.jl
+++ docs/make.jl
@@ -5,3 +5,9 @@ using Documenter, EcologicalNetwork
 makedocs(
     modules = [EcologicalNetwork]
 )
+
+deploydocs(
+    deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
+    repo   = "github.com/PoisotLab/EcologicalNetwork.jl.git",
+    julia  = "0.6"
+)
