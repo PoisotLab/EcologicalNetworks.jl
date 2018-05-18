94e15fa37dc37f05f702ddb1142c993e1170f0ea
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-05-18 11:57:41 -0400

    :memo: documentation for shuffle

--- src/rand/swaps.jl
+++ src/rand/swaps.jl
@@ -16,6 +16,30 @@ function same_fill(x, y)
   return true
 end
 
+"""
+    shuffle(N::BinaryNetwork; constraint::Symbol=:degree, number_of_swaps::Int64=1000, size_of_swap=(3,3), number_of_candidates::Int64=100)
+
+Returns a new network with its interactions shuffled under a given constraint.
+The possible constraints are:
+
+- `:degree`, which keeps the degree distribution intact
+- `:generality`, which keeps the out-degree distribution intact
+- `:vulnerability`, which keeps the in-degree distribution intact
+- `:fill`, which moves interactions around freely
+
+Note that this function will conserve the degree (when appropriate under the
+selected constraint) *of every species*.
+
+This function takes sub-matrices of size `size_of_swap` (the value of
+`size_of_swap` is anything which is accepted by thee `sample` function), and
+shuffle them around `number_of_swaps` time. At every step, there are
+`number_of_candidates` shuffled matrices generated, and the first that matches
+the constraints is used.
+
+The size and connectance of the network, as well as the performance of the
+computer used, should be considered when modifying the values of these
+parameters.
+"""
 function shuffle(N::BinaryNetwork; constraint::Symbol=:degree, number_of_swaps::Int64=1000, size_of_swap=(3,3), number_of_candidates::Int64=100)
     @assert constraint ∈ [:degree, :generality, :vulnerability, :fill]
     @assert number_of_swaps > 0
