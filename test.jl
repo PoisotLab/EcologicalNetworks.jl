0809e39c300a62062324642951ccf635260d0790
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-05-23 12:12:25 -0400

    :hand: test

--- test.jl
+++ test.jl
@@ -10,28 +10,10 @@ begin
 end
 
 N = simplify(first(nz_stream_foodweb()))
-Y = copy(N)
+Y = rand(null2(N), 10000)
+Y = simplify.(Y)
+Y = filter(y -> links(y) == links(N), Y)
 
-@time Y = shuffle(Y)
+m = map(x -> length(find_motif(N, unipartitemotifs()[:S2])), Y)
 
-n = interactions(N)
-a = Array{last(eltype(N))}(length(n),2)
-for i in eachindex(n)
-    a[i,1] = n[i].from
-    a[i,2] = n[i].to
-end
-todo = true
-while todo
-    a[:,1] = shuffle(a[:,1])
-    todo = length(unique(a[:,1].*a[:,2])) != links(N)
-end
-
-A = zeros(Bool, size(N))
-for i in 1:size(a,1)
-    from, to = a[i,:]
-    i1, i2 = findfirst(species(N,1), from), findfirst(species(N,2), to)
-    A[i1,i2] = true
-end
-
-sum(A)
-links(N)
+m0 = length(find_motif(N, unipartitemotifs()[:S2]))
