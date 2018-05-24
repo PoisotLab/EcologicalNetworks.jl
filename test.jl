05863c662cc7f23de568e667f5b5017725f5a8bf
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-05-24 09:31:02 -0400

    :hand:

--- test.jl
+++ test.jl
@@ -2,6 +2,7 @@ include("./src/EcologicalNetwork.jl")
 using EcologicalNetwork
 using StatsBase
 using NamedTuples
+using Combinatorics
 
 begin
     using Base.Test
@@ -10,10 +11,31 @@ begin
 end
 
 N = simplify(first(nz_stream_foodweb()))
-Y = rand(null2(N), 10000)
-Y = simplify.(Y)
-Y = filter(y -> links(y) == links(N), Y)
 
-m = map(x -> length(find_motif(N, unipartitemotifs()[:S2])), Y)
+function internal_shuffle(S::BinaryNetwork, f)
+    collection = [typeof(S)(reshape(shuffle(vec(S.A)), size(S)), species(S)) for i in 1:1000]
+    for c in collection
+        if (f(c) == f(S))&(c.A != S.A)&(links(c)==links(nodiagonal(c)))
+            return c
+        end
+    end
+    info("hyuck")
+    return S
+end
 
-m0 = length(find_motif(N, unipartitemotifs()[:S2]))
+Y = copy(N)
+
+@time begin
+    S = sample(Y, 3)
+    while isdegenerate(S)
+        S = sample(Y, 3)
+    end
+    repl = internal_shuffle(S, degree_in)
+    p1 = indexin(species(repl,1), species(Y,1))
+    p2 = indexin(species(repl,2), species(Y,2))
+    Y.A[p1,p2] = repl.A
+end
+
+heatmap(N.A .- Y.A, aspectratio=1)
+
+degree_in(Y) == degree_in(N)
