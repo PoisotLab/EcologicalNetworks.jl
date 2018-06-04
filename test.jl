228338570b4a30fb5f9851bc36a335fa0318e01b
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-06-04 14:41:33 -0400

    :hand: attempting to work on #96 (but not working)

--- test.jl
+++ test.jl
@@ -4,22 +4,39 @@ using EcologicalNetwork
 using StatsBase
 using NamedTuples
 using Combinatorics
+using Base.Test
 
-begin
-    using JuliaDB
-    using Base.Test
-    using Distributions
-    using Plots, StatPlots
+N = web_of_life("A_HP_001")
+N = nz_stream_foodweb()[1]
+N = convert(BinaryNetwork, N)
+
+s = (6,6)
+
+seed = sample(interactions(N))
+sp = typeof(N) <: AbstractBipartiteNetwork ? ([seed.from], [seed.to]) : ([seed.from, seed.to],)
+n = N[sp...]
+
+# What can we add?
+possible_additions = filter(i -> (i.from ∈ species(n,1))|(i.to ∈ species(n,2)), interactions(N))
+filter!(i -> !(i ∈ interactions(n)), possible_additions)
+
+add = length(possible_additions) == 0 ? sample(interactions(N)) : sample(possible_additions)
+tentative_sp = typeof(N) <: AbstractBipartiteNetwork ? (vcat(species(n,1), [add.from]), vcat(species(n,2), [add.to])) : (unique(vcat(species(n), [add.from, add.to])),)
+tentative_n = N[tentative_sp...]
+
+if (richness(tentative_n, 1) <= first(s))&(richness(tentative_n, 2) <= last(s))
+    sp = deepcopy(tentative_sp)
+    n = N[sp...]
 end
 
-# Important for benchmark
-srand(2000)
-
-ref_net = simplify(BipartiteNetwork(rand(Bool, (20,15))))
-while richness(ref_net) != 35
-    ref_net = simplify(BipartiteNetwork(rand(Bool, (20,15))))
+@time begin
+    t = [reshape(sample(n.A, prod(size(n.A)), replace=false), size(n)) for i in 1:500000]
+    t = unique(t)
+    t = filter(x -> x != n.A, t)
+    m = [typeof(n)(x, EcologicalNetwork.species_objects(n)...) for x in t]
+    k1 = filter(z -> degree(z,1) == degree(n, 1), m)
+    k2 = filter(z -> degree(z,2) == degree(n, 2), m)
+    k12 = intersect(k1,k2)
 end
 
-
-Profile.clear()
-@profiler find_motif(ref_net, BipartiteNetwork([true true; false true]))
+n.A
