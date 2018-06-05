ecf04408f220c1b3d87c32f426a271cbc0430cec
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-06-05 14:42:09 -0400

    :hand: tests.jl

--- test.jl
+++ test.jl
@@ -7,8 +7,35 @@ using Combinatorics
 using Base.Test
 
 N = convert(BinaryNetwork, web_of_life("A_HP_001"))
-Ud = shuffle(N; number_of_swaps=10, constraint=:degree)
+s = typeof(N)[]
+push!(s, copy(N))
 
-Profile.clear()
-@profile shuffle(N; number_of_swaps=10, constraint=:degree)
-Juno.profiler()
+for i in 2:200
+  push!(s, shuffle(s[i-1]; constraint=:degree, number_of_swaps=1))
+end
+
+m_null = BipartiteNetwork([false false; false false])
+m_zeta = BipartiteNetwork([true false; true true])
+m_bicl = BipartiteNetwork([true true; true true])
+m_appa = BipartiteNetwork([false false; true true])
+m_dire = BipartiteNetwork([true false; true false])
+
+f(n,m) = length(find_motif(n,m))
+
+o = zeros(Int64, (length(s), 5))
+@progress for i in eachindex(s)
+  o[i,1] = f(s[i], m_null)
+  o[i,2] = f(s[i], m_appa)
+  o[i,3] = f(s[i], m_dire)
+  o[i,4] = f(s[i], m_zeta)
+  o[i,5] = f(s[i], m_bicl)
+end
+
+O = copy(o)
+O = convert.(Float64, O)
+for i in 1:size(O,2)
+  println(i)
+  O[:,i] = (O[:,i].-O[1,i])./(O[1,i])
+end
+
+heatmap(O', c=:PRGn, aspectratio=10, clim=(-0.15,0.15))
