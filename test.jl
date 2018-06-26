7b9e64b9922c4605b1ffdad77949f7998e8c3a74
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-06-26 13:10:28 -0400

    :wrench: test file

--- test.jl
+++ test.jl
@@ -9,6 +9,33 @@ using Base.Test
 N = convert(BinaryNetwork, web_of_life("A_HP_001"))
 
 Profile.clear()
-@time nodf(N)
-[@profile nodf(N) for i in 1:1000]
+@time shuffle(N, number_of_swaps=5)
+@profile shuffle(N, number_of_swaps=50)
+Juno.profiler()
+
+function interactions2(N::AbstractEcologicalNetwork)
+  edges_accumulator = NamedTuple[]
+  for s1 in species(N,1)
+    for s2 in species(N,2)
+      if has_interaction(N, s1, s2)
+        fields = [:from, :to]
+        values = Any[s1, s2]
+        if typeof(N) <: ProbabilisticNetwork
+          push!(fields, :probability)
+          push!(values, N[s1,s2])
+        end
+        if typeof(N) <: QuantitativeNetwork
+          push!(fields, :strength)
+          push!(values, N[s1,s2])
+        end
+        int_nt = NamedTuples.make_tuple(fields)(tuple(values...)...)
+        push!(edges_accumulator, int_nt)
+      end
+    end
+  end
+  return unique(edges_accumulator)
+end
+
+Profile.clear()
+@profile [interactions2(N) for i in 1:1000]
 Juno.profiler()
