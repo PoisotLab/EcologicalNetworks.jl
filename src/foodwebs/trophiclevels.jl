b4c88634e01842cfa7cb2fbf08e5bfd68f1a09a7
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-07-01 15:32:20 -0400

    :tada: fix the issue with trophic length (#98)
    The fractional trophic level is the maximal distance to any consumer,
    then trophic level is the average ftl

--- src/foodwebs/trophiclevels.jl
+++ src/foodwebs/trophiclevels.jl
@@ -1,33 +1,9 @@
 function fractional_trophic_level(N::T) where {T<:UnipartiteNetwork}
   Y = nodiagonal(N)
-  tl = Dict(zip(species(Y), zeros(Int64, richness(Y))))
-  d_in, d_out = degree_in(Y), degree_out(Y)
-  primary_producers = collect(keys(filter((k,v) -> v == 0, d_out)))
-  for pp_sp in primary_producers
-    tl[pp_sp] = 1
-  end
-
-  updated = true
-  current_tl = 1
-
-  while updated
-    updated = false
-    preys_of_current_rank = filter(s -> tl[s].==current_tl, species(Y))
-    if length(preys_of_current_rank) > 0
-      updated = true
-      for s in species(Y,1)
-        for p in preys_of_current_rank
-          if has_interaction(Y, s, p)
-            if !(has_interaction(Y, p, s))
-              tl[s] = current_tl+1
-            end
-          end
-        end
-      end
-    end
-    current_tl = current_tl+1
-  end
-
+  producers = keys(filter((sp,de) -> de == 0, degree(Y,1)))
+  sp = shortest_path(Y)
+  prod_id = find(sum(sp,2).==0)
+  tl = Dict(zip(species(Y,1), maximum(sp[:,prod_id],2).+1))
   return tl
 end
 
--- test.jl
+++ test.jl
@@ -1,4 +1,3 @@
-using Revise # To avoid reloading the session while we test things
 include("./src/EcologicalNetwork.jl")
 using EcologicalNetwork
 using StatsBase
@@ -6,30 +5,3 @@ using NamedTuples
 using Combinatorics
 using Base.Test
 using StatPlots
-
-N = UnipartiteNetwork([false true true; true false true; false false false])
-fractional_trophic_level(N)
-
-Ns = [UnipartiteNetwork(rand(Bool, (10,10))) for i in 1:100]
-N = Ns[1]
-@progress for N in Ns
-    info("ping")
-    fractional_trophic_level(N)
-end
-
-
-A = [0 0 0 0 ; 1 0 0 0; 1 0 0 1; 0 1 1 0]
-N = UnipartiteNetwork(A.>0)
-
-function nonzeromean(x)
-  if sum(x) == 0
-    return 0.0
-  end
-  return mean(x[x.>0])
-end
-
-Y = copy(nodiagonal(N))
-producers = keys(filter((sp,de) -> de == 0, degree(Y,1)))
-sp = shortest_path(Y)
-prod_id = find(sum(sp,2).==0)
-tl = Dict(zip(species(Y,1), mean(sp[:,prod_id],2).+1.0))
