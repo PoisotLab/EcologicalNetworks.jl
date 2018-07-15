254c39a663c7395ad9faf137a96ebb700f19b574
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-07-15 11:10:59 -0400

    :bug: wrong name used for type

--- src/types/comparisons.jl
+++ src/types/comparisons.jl
@@ -1,42 +1,42 @@
 import Base: <, ≤
 
-function <(N::T, n::T) where {T <: QuantitativeNetwork, n <: Number}
+function <(n::NT, n::NT) where {T <: QuantitativeNetwork, NT <: Number}
    ReturnType = T <: AbstractBipartiteNetwork ? BipartiteNetwork : UnipartiteNetwork
    return ReturnType(N.A .< n, species_objects(N)...)
 end
 
-function <(N::T, n::T) where {T <: ProbabilisticNetwork, n <: AbstractFloat}
+function <(n::NT, n::NT) where {T <: ProbabilisticNetwork, NT <: AbstractFloat}
    ReturnType = T <: AbstractBipartiteNetwork ? BipartiteNetwork : UnipartiteNetwork
    return ReturnType(N.A .< n, species_objects(N)...)
 end
 
-function <(n::T, N::T) where {T <: QuantitativeNetwork, n <: Number}
+function <(n::NT, n::NT) where {T <: QuantitativeNetwork, NT <: Number}
    ReturnType = T <: AbstractBipartiteNetwork ? BipartiteNetwork : UnipartiteNetwork
    return ReturnType(n .< N.A, species_objects(N)...)
 end
 
-function <(n::T, N::T) where {T <: ProbabilisticNetwork, n <: AbstractFloat}
+function <(n::NT, n::NT) where {T <: ProbabilisticNetwork, NT <: AbstractFloat}
    ReturnType = T <: AbstractBipartiteNetwork ? BipartiteNetwork : UnipartiteNetwork
    return ReturnType(n .< N.A, species_objects(N)...)
 end
 
 
-function ≤(N::T, n::T) where {T <: QuantitativeNetwork, n <: Number}
+function ≤(n::NT, n::NT) where {T <: QuantitativeNetwork, NT <: Number}
    ReturnType = T <: AbstractBipartiteNetwork ? BipartiteNetwork : UnipartiteNetwork
    return ReturnType(N.A .≤ n, species_objects(N)...)
 end
 
-function ≤(N::T, n::T) where {T <: ProbabilisticNetwork, n <: AbstractFloat}
+function ≤(n::NT, n::NT) where {T <: ProbabilisticNetwork, NT <: AbstractFloat}
    ReturnType = T <: AbstractBipartiteNetwork ? BipartiteNetwork : UnipartiteNetwork
    return ReturnType(N.A .≤ n, species_objects(N)...)
 end
 
-function ≤(n::T, N::T) where {T <: QuantitativeNetwork, n <: Number}
+function ≤(n::NT, n::NT) where {T <: QuantitativeNetwork, NT <: Number}
    ReturnType = T <: AbstractBipartiteNetwork ? BipartiteNetwork : UnipartiteNetwork
    return ReturnType(n .≤ N.A, species_objects(N)...)
 end
 
-function ≤(n::T, N::T) where {T <: ProbabilisticNetwork, n <: AbstractFloat}
+function ≤(n::NT, n::NT) where {T <: ProbabilisticNetwork, NT <: AbstractFloat}
    ReturnType = T <: AbstractBipartiteNetwork ? BipartiteNetwork : UnipartiteNetwork
    return ReturnType(n .≤ N.A, species_objects(N)...)
 end
