dcdecb2ece6bf2b8716b8342fe390854c1ac1b79
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-08-01 15:46:44 -0400

    :hand:

--- src/foodwebs/trophicspecies.jl
+++ src/foodwebs/trophicspecies.jl
@@ -1,3 +1,7 @@
 function trophicspecies(N::T; clustering_function::Function=EAJS) where {T <: UnipartiteNetwork}
+
     similarities = clustering_function(N)
+
+    # TODO make a matrix based on overlap
+
 end
