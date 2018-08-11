4d3ce4698e5302a92fdc58a8022d62249279114e
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-08-11 12:26:09 -0400

    :tada: add (currently non-exported) linearfilter function

--- src/rand/null.jl
+++ src/rand/null.jl
@@ -1,3 +1,36 @@
+function linearfilter(N::T; α::Vector{Float64}=fill(0.25, 4)) where {T <: BinaryNetwork}
+  @assert length(α) == 4
+  α = α./sum(α) # This ensures that α sums to 1.0
+
+  # Get the size of the network
+  n = richness(N; dims=1)
+  m = richness(N; dims=2)
+
+  # Get the in and out degree, as well as the total number of interactions
+  k_out = degree(N; dims=1)
+  k_in = degree(N; dims=2)
+  A = sum(N)
+
+  # This line is not optimal because it prevents type stability in the output,
+  # but this is sufficient for the illustration
+  return_type = T <: AbstractBipartiteNetwork ? BipartiteProbabilisticNetwork : UnipartiteProbabilisticNetwork
+  F = return_type(
+    zeros(Float64, size(N)),
+    EcologicalNetworks.species_objects(N)...
+  )
+
+  # This loop is made possible by the fact that network objects can be modified.
+  # Rather than looping on species positions, we can loop directly on species
+  # names, and get the informations from the dictionaries with the degrees.
+  for s1 in species(N; dims=1)
+    for s2 in species(N; dims=2)
+      F[s1,s2] = α[1]*N[s1,s2] + (α[2]/m)*k_out[s1] + (α[3]/n)*k_in[s2] + α[4]/(n*m)*A
+    end
+  end
+
+  return F
+end
+
 """
     null1(N::BinaryNetwork)
 
