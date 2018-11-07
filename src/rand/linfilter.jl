e77b3e4738957ba6eede0167bddfcc42d6385a5a
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-11-07 15:45:55 -0500

    :bug: fix the rounding issues in linearfilter #115

--- src/rand/linfilter.jl
+++ src/rand/linfilter.jl
@@ -30,7 +30,8 @@ function linearfilter(N::T; α::Vector{Float64}=fill(0.25, 4)) where {T <: Binar
   # Get probabilities
   for s1 in species(N; dims=1)
     for s2 in species(N; dims=2)
-      F[s1,s2] = α[1]*N[s1,s2] + (α[2]/m)*k_out[s1] + (α[3]/n)*k_in[s2] + α[4]/(n*m)*A
+      t_val = α[1]*N[s1,s2] + (α[2]/m)*k_out[s1] + (α[3]/n)*k_in[s2] + α[4]/(n*m)*A
+      F[s1,s2] = max(min(t_val, 1.0), 0.0)
     end
   end
 
@@ -58,7 +59,8 @@ function linearfilterzoo(N::T; α::Vector{Float64}=fill(0.25, 4)) where {T <: Bi
   # Get probabilities
   for s1 in species(N; dims=1)
     for s2 in species(N; dims=2)
-      Fzoo[s1,s2] = F[s1,s2] - (α[1] + (α[2]/m) + (α[3]/n) + α[4]/(n * m)) * N[s1,s2]
+      t_val = F[s1,s2] - (α[1] + (α[2]/m) + (α[3]/n) + α[4]/(n * m)) * N[s1,s2]
+      Fzoo[s1,s2] = max(min(t_val, 1.0), 0.0)
     end
   end
 
