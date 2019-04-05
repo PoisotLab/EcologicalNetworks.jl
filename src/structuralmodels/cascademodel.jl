457244e5dad96e5ec744401effd986830ddf7a45
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-04-05 09:47:30 -0400

    :tada: null models

--- src/structuralmodels/cascademodel.jl
+++ src/structuralmodels/cascademodel.jl
@@ -1,8 +1,8 @@
 """
-    cascademodel(Species::Int64, Co::Float64)
+    cascademodel(S::Int64, Co::Float64)
 
 Return matrix of the type `UnipartiteNetwork` randomly assembled according to
-the cascade model for a given nuber of `Species` and connectivity `Co`.
+the cascade model for a given nuber of `S` and connectivity `Co`.
 
 > Cohen, J. E. and Newman, C. M. (1985) ‘A stochastic theory of community
 > food webs I. Models and aggregated data’, Proc. R. Soc. Lond. B, 224(1237),
@@ -10,24 +10,24 @@ the cascade model for a given nuber of `Species` and connectivity `Co`.
 
 See also: `nichemodel`, `mpnmodel`, `nestedhierarchymodel`
 """
-function cascademodel(Species::Int64, Co::Float64)
+function cascademodel(S::Int64, Co::Float64)
 
     # Evaluate input
-    maxconnectance = ((Species^2-Species)/2)/(Species*Species)
-    Co >= maxconnectance && throw(ArgumentError("Connectance for $(Species) species cannot be larger than $(maxconnectance) "))
+    maxconnectance = ((S^2-S)/2)/(S*S)
+    Co >= maxconnectance && throw(ArgumentError("Connectance for $(S) species cannot be larger than $(maxconnectance) "))
     Co <= 0 && throw(ArgumentError("Connectance C must be positive"))
-    Species <= 0 && throw(ArgumentError("Number of species L must be positive"))
+    S <= 0 && throw(ArgumentError("Number of species L must be positive"))
 
     # Initiate matrix
-    A = UnipartiteNetwork(zeros(Bool, (Species, Species)))
+    A = UnipartiteNetwork(zeros(Bool, (S, S)))
 
     # For each species randomly asscribe rank e
-    e = sort(rand(Species), rev=false)
+    e = sort(rand(S), rev=false)
 
     # Probability for linking two species
-    p = 2*Co*Species/(Species - 1)
+    p = 2*Co*S/(S - 1)
 
-    for consumer in  1:Species
+    for consumer in  1:S
 
         # Rank for a consumer
         rank = e[consumer]
@@ -67,15 +67,13 @@ function cascademodel(N::T) where {T <: UnipartiteNetwork}
 end
 
 """
-    cascademodel(Species::Int64, L::Int64)
+    cascademodel(S::Int64, L::Int64)
 
 Number of links can be specified instead of connectance.
 """
-function cascademodel(Species::Int64, L::Int64)
-
-    Co = (L/(Species*Species))
-
-    return cascademodel(Species, Co)
+function cascademodel(S::Int64, L::Int64)
+    Co = (L/(S*S))
+    return cascademodel(S, Co)
 
 end
 
@@ -83,8 +81,9 @@ end
 
     cascademodel(parameters::Tuple)
 
-Parameters tuple can also be provided in the form (Species::Int64, Co::Float64)
-or (Species::Int64, Int::Int64).
+Parameters tuple can also be provided in the form (S::Int64, Co::Float64)
+or (S::Int64, L::Int64).
+
 """
 function cascademodel(parameters::Tuple)
     return cascademodel(parameters[1], parameters[2])
