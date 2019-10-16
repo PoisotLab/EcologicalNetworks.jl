693bf1e957e82f38036a1619d207461eeb428cf0
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-10-15 20:53:56 -0400

    :whiskey: add the spectral radius code in #147
    (also sipping Laphroaig so code may be bad)

--- src/community/spectralradius.jl
+++ src/community/spectralradius.jl
@@ -1,6 +1,8 @@
 function ρ(N::T; range::Bool=true) where {T <: AbstractUnipartiteNetwork}
     @assert minimum(N.A) ≥ zero(eltype(N.A))
-
+    absolute_real_part = abs.(real.(eigvals(N.A)))
+    range && return maximum(absolute_real_part)/norm(N.A)
+    return maximum(absolute_real_part)
 end
 
 function ρ(N::T; varargs...) where {T <: AbstractBipartiteNetwork}
