bbbe47998ee01c6efd47857d7d7d673d71f9a930
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-02-10 14:53:28 -0500

    Closes #203

--- test/community/spectralradius.jl
+++ test/community/spectralradius.jl
@@ -20,9 +20,12 @@ A = [1 0 1 1; 1 1 0 1; 1 1 1 0; 1 1 1 0; 1 1 1 0; 0 1 0 1]
 N = BipartiteNetwork(A.>0)
 @test ρ(N; range=EcologicalNetworks.ρ_raw) ≈ 3.595 atol = 0.01
 
+# This specific example was added to catch a bug due to the wrong
+# normalization, see issue #203
+
 A = [1 1 1 1; 1 1 1 0; 1 1 1 0; 1 1 1 0; 1 1 1 0; 1 0 0 0]
-N = BipartiteNetwork(A)
-@test ρ(N, range=ρ_raw) ≈ 3.943904 atol = 0.01
-@test ρ(N, range=ρ_ska) ≈ 0.956537 atol = 0.01
+N = BipartiteNetwork(A.>0)
+@test ρ(N, range=EcologicalNetworks.ρ_raw) ≈ 3.943904 atol = 0.01
+@test ρ(N, range=EcologicalNetworks.ρ_ska) ≈ 0.956537 atol = 0.01
 
 end
