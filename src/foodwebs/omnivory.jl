93037e5a7d291c32dd98dc57c1e526190155ed15
Matthew J. Michalska-Smith <mjsmith037@gmail.com>
2022-02-09 20:46:00 -0500

    :bug: fixed omnivory to use true trophic level and iterate across prey rather than predators

--- src/foodwebs/omnivory.jl
+++ src/foodwebs/omnivory.jl
@@ -1,8 +1,8 @@
 function omnivory(N::T) where {T <: UnipartiteNetwork}
     OI = Dict([s => 0.0 for s in species(N)])
 
-    TL = fractional_trophic_level(N)
-    k = degree(N; dims=1)
+    TL = trophic_level(N)
+    k = degree_in(N)
 
     for sp_i in species(N)
 
@@ -11,10 +11,10 @@ function omnivory(N::T) where {T <: UnipartiteNetwork}
 
         # For every species, we set its initial omnivory to 0
         oi = 0.0
-        for (j, sp_j) in enumerate(species(N))
+        for sp_j in species(N)
             # Then for every species it consumes, we ha
             tl_diff = (TL[sp_j] - (TL[sp_i]-1.0)).^2.0
-            corr = N[sp_i,sp_j]/k[sp_i]
+            corr = N[sp_j,sp_i]/k[sp_i]
             oi += tl_diff * corr
         end
         OI[sp_i] = oi
