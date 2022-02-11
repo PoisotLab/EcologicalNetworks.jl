86ad20a5e7b6c105feb71c064be4aa6217ba645b
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-02-11 12:21:23 -0500

    test fix

--- test/community/foodwebs.jl
+++ test/community/foodwebs.jl
@@ -4,8 +4,8 @@ module TestFoodWebs
 
   # test trophic levels on motifs
   s1 = unipartitemotifs()[:S1]
-  s1_ftl = fractional_trophic_level(s1)
-  s1_tl = trophic_level(s1)
+  s1_ftl = trophic_level(s1)
+  s1_tl = distance_to_producer(s1)
   @test maximum(values(s1_ftl)) ≈ 3
   @test maximum(values(s1_tl)) ≈ 3.0
   @test s1_ftl[:a] ≈ 3
@@ -13,8 +13,8 @@ module TestFoodWebs
   @test s1_ftl[:c] ≈ 1
 
   s2 = unipartitemotifs()[:S2]
-  @test minimum(values(fractional_trophic_level(s1))) ≈ 1.0
-  @test minimum(values(trophic_level(s1))) == 1.0
+  @test minimum(values(trophic_level(s1))) ≈ 1.0
+  @test minimum(values(distance_to_producer(s1))) == 1.0
 
   # test on a quantitative bipartite network
   @test web_of_life("A_HP_002") |>
