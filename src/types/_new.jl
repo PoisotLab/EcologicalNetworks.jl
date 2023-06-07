7ce8edd1e578417ca443f6a92bee8708f2148a85
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-06-07 11:09:11 -0400

    test(types): add tests for partiteness

--- src/types/_new.jl
+++ src/types/_new.jl
@@ -9,8 +9,18 @@ struct Bipartite{T <: Any} <: Partiteness
     bottom::Vector{T}
 end
 
+@testitem "We can declare a bipartite species set with symbols" begin
+    set = Bipartite([:a, :b, :c], [:A, :B, :C])
+    @test richness(set) == 6
+end
+
 struct Unipartite{T <: Any} <: Partiteness
     margin::Vector{T}
+end
+
+@testitem "We can declare a unipartite species set with symbols" begin
+    set = Unipartite([:a, :b, :c])
+    @test richness(set) == 3
 end
 
 struct Probabilistic{T <: AbstractFloat} <: Interactions
@@ -38,7 +48,9 @@ end
     @test species(N) == [:a, :b, :c]
 end
 
-species(N::SpeciesInteractionNetwork{P,E}) where {P<:Bipartite, E<:Interactions} = vcat(N.nodes.top, N.nodes.bottom)
-species(N::SpeciesInteractionNetwork{P,E}) where {P<:Unipartite, E<:Interactions} = N.nodes.margin
+species(N::Bipartite) = vcat(N.top, N.bottom)
+species(N::Unipartite) = N.margin
+species(N::SpeciesInteractionNetwork) = species(N.nodes)
 
-richness(N::SpeciesInteractionNetwork) = length(species(N))
+richness(N::Partiteness) = length(species(N))
+richness(N::SpeciesInteractionNetwork) = length(species(N.nodes))
