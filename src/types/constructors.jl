b356c0d06f6879116c8c0fd1fe977e0cbcbb7053
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-06-08 13:43:00 -0400

    refactor(types): simplify the external constructors

--- src/types/constructors.jl
+++ src/types/constructors.jl
@@ -1,12 +1,61 @@
-function SpeciesInteractionNetwork{Bipartite, Probabilistic}(
-    M::Matrix{T},
-) where {T <: Number}
-    ric = size(M)
-    top = Symbol.("top_" .* string.(1:ric[1]))
-    bottom = Symbol.("bottom_" .* string.(1:ric[2]))
-    edges = Probabilistic(sparse(M))
-    nodes = Bipartite(top, bottom)
-    return SpeciesInteractionNetwork{Bipartite, Probabilistic}(nodes, edges)
+Probabilistic(M::Matrix{T}) where {T <: AbstractFloat} = Probabilistic(dropzeros(sparse(M)))
+
+@testitem "We can construct probabilistic edges from a matrix of floats" begin
+    M = rand(Float64, (10, 10))
+    @test eltype(Probabilistic(M)) == eltype(M)
+end
+
+Binary(M::Matrix{T}) where {T <: Bool} = Binary(dropzeros(sparse(M)))
+Binary(M::BitMatrix) = Binary(dropzeros(sparse(M)))
+
+@testitem "We can construct edges from a matrix of booleans" begin
+    M = rand(Bool, (10, 10))
+    @test eltype(Binary(M)) == eltype(M)
+end
+
+@testitem "We can construct edges from a matrix of bits" begin
+    M = rand(Float64, (10, 10)) .< 0.2
+    @test eltype(Binary(M)) == eltype(M)
+end
+
+Quantitative(M::Matrix{T}) where {T <: Number} = Quantitative(dropzeros(sparse(M)))
+
+@testitem "We can construct quantitative edges from a matrix of numbers" begin
+    M = floor.(Int64, rand(Float64, (10, 10)).*10.0)
+    @test eltype(Quantitative(M)) == eltype(M)
+end
+
+function Bipartite(E::Interactions)
+    rt, rb = size(E.edges)
+    top = Symbol.("top_" .* string.(1:rt))
+    bottom = Symbol.("bottom_" .* string.(1:rb))
+    return Bipartite(top, bottom)
+end
+
+function Unipartite(E::Interactions)
+    rt, rb = size(E.edges)
+    if rt != rb
+        throw(DimensionMismatch("The edges object has size $(rt), $(rb)"))
+    end
+    nodes = Symbol.("node_" .* string.(1:rt))
+    return Unipartite(nodes)
+end
+
+@testitem "We can construct a unipartite set from an interaction struct" begin
+    E = Binary(rand(Bool, (4, 4)))
+    S = Unipartite(E)
+    @test richness(S) == 4
+end
+
+@testitem "We cannot construct a unipartite set from an interaction struct with different sizes" begin
+    E = Binary(rand(Bool, (4, 5)))
+    @test_throws DimensionMismatch Unipartite(E)
+end
+
+function SpeciesInteractionNetwork{P, E}(M::Matrix{T}) where {T, P <: Partiteness, E <: Interactions}
+    edges = E(M)
+    nodes = P(edges)
+    return SpeciesInteractionNetwork{P,E}(nodes, edges)
 end
 
 @testitem "We can construct a bipartite probabilistic network from a matrix" begin
@@ -17,30 +66,12 @@ end
     @test eltype(N.nodes) == Symbol
 end
 
-function SpeciesInteractionNetwork{Unipartite, Probabilistic}(
-    M::Matrix{T},
-) where {T <: Number}
-    ric = size(M)
-    nodes = Unipartite(Symbol.("node_" .* string.(1:ric[1])))
-    edges = Probabilistic(sparse(M))
-    return SpeciesInteractionNetwork{Unipartite, Probabilistic}(nodes, edges)
-end
-
 @testitem "We can construct a unipartite probabilistic network from a matrix" begin
     M = rand(Float64, (12, 12))
     N = SpeciesInteractionNetwork{Unipartite, Probabilistic}(M)
     @test richness(N) == size(M, 1)
     @test eltype(N.edges) == eltype(M)
     @test eltype(N.nodes) == Symbol
-end
-
-function SpeciesInteractionNetwork{Unipartite, Binary}(
-    M::Matrix{T},
-) where {T <: Bool}
-    ric = size(M)
-    nodes = Unipartite(Symbol.("node_" .* string.(1:ric[1])))
-    edges = Binary(sparse(M))
-    return SpeciesInteractionNetwork{Unipartite, Binary}(nodes, edges)
 end
 
 @testitem "We can construct a unipartite binary network from a matrix" begin
