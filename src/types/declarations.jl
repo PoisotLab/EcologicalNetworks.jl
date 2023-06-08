71babade0d2d720e75d61f3dfe52d5006f63a38b
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-06-08 13:52:33 -0400

    test: internal constructors

--- src/types/declarations.jl
+++ src/types/declarations.jl
@@ -12,6 +12,18 @@ using TestItems
 struct Bipartite{T <: Any} <: Partiteness
     top::Vector{T}
     bottom::Vector{T}
+    function Bipartite(top::Vector{T}, bottom::Vector{T}) where {T <: Any}
+        if ~allunique(top)
+            throw(ArgumentError("The species in the top level of a bipartite network must be unique"))
+        end
+        if ~allunique(bottom)
+            throw(ArgumentError("The species in the bottom level of a bipartite network must be unique"))
+        end
+        if ~allunique(vcat(bottom, top))
+            throw(ArgumentError("The species in a bipartite network cannot appear in both levels"))
+        end
+        return new{T}(top, bottom)
+    end
 end
 
 @testitem "We can construct a bipartite species set with symbols" begin
@@ -34,6 +46,12 @@ end
 
 struct Unipartite{T <: Any} <: Partiteness
     margin::Vector{T}
+    function Unipartite(margin::Vector{T}) where {T <: Any}
+        if ~allunique(margin)
+            throw(ArgumentError("The species in a unipartite network must be unique"))
+        end
+        return new{T}(margin)
+    end
 end
 
 @testitem "We can construct a unipartite species set with symbols" begin
@@ -64,8 +82,8 @@ end
 
 @testitem "We can construct a unipartite probabilistic network" begin
     nodes = Unipartite([:a, :b, :c])
-    edges = Binary(sparse(rand(Bool, (3,3))))
+    edges = Binary(rand(Bool, (3,3)))
     N = SpeciesInteractionNetwork(nodes, edges)
-    @test richnes(N) == 3
+    @test richness(N) == 3
     @test species(N) == [:a, :b, :c]
 end
