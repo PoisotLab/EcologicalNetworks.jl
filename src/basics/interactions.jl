89e0c21134b59cd2d8ec3c909de773d51f85feca
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-06-10 15:05:41 -0400

    test: empty graph interactions

--- src/basics/interactions.jl
+++ src/basics/interactions.jl
@@ -6,3 +6,10 @@ interactions(N::SpeciesInteractionNetwork) = [int for int in N]
     N = SpeciesInteractionNetwork(nodes, edges)
     @test interactions(N) == [(:A, :c, true), (:A, :d, true), (:B, :d, true)]
 end
+
+@testitem "An empty graph returns and empty interaction list" begin
+    nodes = Bipartite([:A, :B], [:c, :d])
+    edges = Binary([false false; false false])
+    N = SpeciesInteractionNetwork(nodes, edges)
+    @test isempty(interactions(N))
+end
